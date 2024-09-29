35a312cf476ee30b08c97b2cfb5d968e9faf6bcb
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2024-09-28 19:51:49 -0500

    Time: 36 ms (60.43%), Space: 16.5 MB (17.56%) - LeetHub

--- 3379-score-of-a-string/3379-score-of-a-string.py
+++ 3379-score-of-a-string/3379-score-of-a-string.py
@@ -1,11 +1,9 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
-        p1=0
         p2=1
         l=[]
         while p2<len(s):
-            l.append(abs(ord(s[p1])-ord(s[p2])))
-            p1+=1
+            l.append(abs(ord(s[p2-1])-ord(s[p2])))
             p2+=1
         return sum(l)
         
