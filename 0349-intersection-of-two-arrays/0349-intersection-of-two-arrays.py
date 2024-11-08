d1512160c0a14f86094e71a7b9d035d92ecbf23d
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2024-11-08 10:35:19 -0600

    Time: 0 ms (100%), Space: 16.7 MB (80.78%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.py
@@ -3,18 +3,18 @@ class Solution:
         '''
         i can solve it by two pointer
         '''
-        # nums1=set(nums1)
-        # nums2=set(nums2)
-        # return list(nums1 & nums2)
+        nums1=set(nums1)
+        nums2=set(nums2)
+        return list(nums1 & nums2)
 
-        res =[]
-        p1=0
-        while p1<len(nums1):
-            p2=0
-            while p2<len(nums2):
-                if ((nums1[p1]==nums2[p2]) and(nums1[p1] not in res)):
-                    res.append(nums1[p1])
-                p2+=1
-            p1+=1
-        return res
+        # res =[]
+        # p1=0
+        # while p1<len(nums1):
+        #     p2=0
+        #     while p2<len(nums2):
+        #         if ((nums1[p1]==nums2[p2]) and(nums1[p1] not in res)):
+        #             res.append(nums1[p1])
+        #         p2+=1
+        #     p1+=1
+        # return res
         
