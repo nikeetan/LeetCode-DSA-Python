91d9f10c62c9662063aef86acbdd44402161e5a1
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2024-11-20 12:16:07 -0600

    Time: 272 ms (37.78%), Space: 17.2 MB (55.21%) - LeetHub

---
+++ 2599-take-k-of-each-character-from-left-and-right/2599-take-k-of-each-character-from-left-and-right.py
@@ -0,0 +1,25 @@
+class Solution:
+    def takeCharacters(self, s: str, k: int) -> int:
+        count_arr=[0,0,0]
+        for i in range(len(s)):
+            count_arr[ord(s[i])-ord('a')]+=1
+
+        if min(count_arr)<k:
+            return -1
+        else:
+            left,right=0,0
+            win_size=float('inf')
+            while right<len(s):
+                count_arr[ord(s[right])-ord('a')]-=1
+                while min(count_arr)<k:
+                    count_arr[ord(s[left])-ord('a')]+=1
+                    left+=1
+                win_size=min(win_size,len(s)-(right-left+1))
+                right+=1
+            print(win_size)
+            return win_size
+
+
+
+
+        
