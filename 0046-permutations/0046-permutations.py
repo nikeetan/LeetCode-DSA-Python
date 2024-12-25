73bcc4c6f4db261888d13fe126d8ff50e6ed2a98
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2024-12-25 04:55:50 -0600

    Time: 0 ms (100%), Space: 17.7 MB (13.87%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,22 +1,34 @@
 class Solution:
-    def helper(self,stack:list[int],bl:list[int],dict1,n:int,nums:List[int]):
-        if n==len(nums):
-            bl.append(stack[:])
+    # def helper(self,stack:list[int],bl:list[int],dict1,n:int,nums:List[int]):
+    #     if n==len(nums):
+    #         bl.append(stack[:])
+    #         return
+    #     for i in range(len(nums)):
+    #         if dict1[nums[i]]!=1:
+    #             dict1[nums[i]]=1
+    #             stack.append(nums[i])
+    #             self.helper(stack,bl,dict1,n+1,nums)
+    #             stack.pop()
+    #             dict1[nums[i]]=0
+    def helper(self,bl:list[int],index:int,nums:list[int]):
+        if index==len(nums):
+            bl.append(nums[:])
             return
-        for i in range(len(nums)):
-            if dict1[nums[i]]!=1:
-                dict1[nums[i]]=1
-                stack.append(nums[i])
-                self.helper(stack,bl,dict1,n+1,nums)
-                stack.pop()
-                dict1[nums[i]]=0
+        for i in range(index,len(nums)):
+            nums[i],nums[index]=nums[index],nums[i]
+            self.helper(bl,index+1,nums)
+            nums[i],nums[index]=nums[index],nums[i]
         
     def permute(self, nums: List[int]) -> List[List[int]]:
-        stack=[]
-        dict1={}
-        for i in nums:
-            dict1[i]=0
+        # stack=[]
+        # dict1={}
+        # for i in nums:
+        #     dict1[i]=0
+        # bl=[]
+        # n=0
+        # self.helper(stack,bl,dict1,n,nums)
+        # return bl
         bl=[]
-        n=0
-        self.helper(stack,bl,dict1,n,nums)
+        index=0
+        self.helper(bl,index,nums)
         return bl
