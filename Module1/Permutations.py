c1f05a99550341448cc444d0a1f3f9cfcedfd73f
Niketan Doddamani <niketandoddamani@gmail.com>
2024-12-29 14:28:34 +0530

    Added optimized permutation

--- Module1/Permutations.py
+++ Module1/Permutations.py
@@ -1,3 +1,14 @@
+def Permutation_Swap(array:list[int],index:int,bl:list[int]):
+    if index==len(array):
+        bl.append(array[:])
+        return
+    else:
+        for i in range(index,len(array)):
+            array[i],array[index]=array[index],array[i]
+            Permutation_Swap(array,index+1,bl)
+            array[i],array[index]=array[index],array[i]
+
+
 def Permutation(array:list[int],stack:list[int],bl:list[int],n:int,marked:dict[int]):
     if n==len(array):
         bl.append(stack[:])
@@ -16,5 +27,8 @@ bl=[]
 marked={}
 for i in range(len(array)):
     marked[array[i]]=0
-Permutation(array,stack,bl,n,marked)
+#Permutation(array,stack,bl,n,marked)
+index=0
+Permutation_Swap(array,index,bl)
 print(bl)
+#print(bl)
