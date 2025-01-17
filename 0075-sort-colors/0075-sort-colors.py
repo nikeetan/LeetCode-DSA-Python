5cfc97ba4d89642dac2ef661ee87ef674433c0d9
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-01-17 05:58:53 -0600

    Time: 0 ms (100%), Space: 17.9 MB (18.2%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,12 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        d={0:0,1:0,2:0}
-        for i in nums:
-            d[i]+=1
-        for i in range(d[0]):
-            nums[i]=0
-        for i in range(d[0],d[0]+d[1]):
-            nums[i]=1
-        for i in range(d[0]+d[1],len(nums)):
-            nums[i]=2
+        start,current,end=0,0,len(nums)-1
+        while current<=end:
+            if nums[current]==0:
+                nums[start],nums[current]=nums[current],nums[start]
+                start+=1
+                current+=1
+            elif nums[current]==1:
+                current+=1
+            elif nums[current]==2:
+                nums[current],nums[end]=nums[end],nums[current]
+                end-=1
+        return nums
+        # d={0:0,1:0,2:0}
+        # for i in nums:
+        #     d[i]+=1
+        # for i in range(d[0]):
+        #     nums[i]=0
+        # for i in range(d[0],d[0]+d[1]):
+        #     nums[i]=1
+        # for i in range(d[0]+d[1],len(nums)):
+        #     nums[i]=2
