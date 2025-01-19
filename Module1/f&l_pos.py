4d3e6067b2607a4d90c6e9f516ed8d9fc2f79f8c
Niketan Doddamani <niketandoddamani@gmail.com>
2025-01-19 07:14:13 -0600

    Modified f&l of bs

--- Module1/f&l_pos.py
+++ Module1/f&l_pos.py
@@ -1,33 +1,40 @@
 def binary_range(arr:list[int],target:int,low:int,high:int):
     # Left Growth
     range_l=[]
-    while low<high:
+    while low<=high:
+        
         mid = low + (high-low)//2
+        print(low,mid,high)
         if arr[mid]==target:
-            high=mid
+            high=mid-1
         elif arr[mid]<target:
             low = mid+1
         else:
             high = mid-1
-    range_l.append(high)
+    if low<=len(arr) and arr[low]==target:
+        range_l.append(low)
     #Right Growth
     low,high=0,len(arr)-1
-    while low<high:
+    while low<=high:
+       
         mid = low + (high-low)//2
+        print(low,mid,high)
         if arr[mid]==target:
-            low=mid
+            low=mid+1
         elif arr[mid]<target:
             low = mid+1
         else:
             high = mid-1
-    range_l.append(low)
+    if high>=0 and arr[high]==target:
+        range_l.append(high)
     return range_l
 
 
 
 
-arr=[3,9,9,9,11,12]
-low,high=0,len(arr)-1
-target=9
+arr =[5,7,7,8,8,10]
+target = 8
+low = 0
+high = len(arr) - 1
 print(binary_range(arr,target,low,high))
 
