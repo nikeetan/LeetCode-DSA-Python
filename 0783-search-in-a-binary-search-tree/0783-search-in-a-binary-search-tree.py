db782a397e1a9a819d5f4885de6e4d47c2f129c8
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-01-24 06:57:22 -0600

    Time: 0 ms (100%), Space: 19.2 MB (72.95%) - LeetHub

---
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        que=[root]
+        to_return=[]
+        while que:
+            for i in range(len(que)):
+                temp=que.pop(0)
+                if temp:
+                    if temp.val==val:
+                        return temp
+                    if val<temp.val:
+                        que.append(temp.left)
+                    else:
+                        que.append(temp.right)
+        return None
