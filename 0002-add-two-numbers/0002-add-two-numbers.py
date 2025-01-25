4733f1ea84afffcbe355ed4323b1a49acd935070
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-01-24 21:59:13 -0600

    Time: 6 ms (37.46%), Space: 17.9 MB (37.66%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -10,34 +10,35 @@ class Solution:
         dh=ListNode(0)
         curr=dh
         carry=0
+        n1=l1
+        prev=None
         while l1 and l2:
             sum1=(l1.val+l2.val+carry)
             carry=(sum1//10)
-            newnode=ListNode(sum1%10)
-            curr.next=newnode
-            curr=curr.next
+            l1.val=sum1%10
+            prev=l1
             l1=l1.next
             l2=l2.next
-            
-        val1,v1l2=0,0
-        while carry!=0:            
-            if l1:
-                val1=l1.val
-                l1=l1.next
-            else:
-                val1=0
+        if ((l1 is None) and (l2 is None) and carry==0):
+            return n1 
+        elif ((l1 is None) and (l2 is None) and carry==1):
+            prev.next=ListNode(1)
+            return n1 
+        else:        
             if l2:
-                val2=l2.val
-                l2=l2.next
-            else:
-                val2=0
-            sum1=val1+val2+carry
-            newnode=ListNode(sum1%10)
-            carry=sum1//10
-            curr.next=newnode
-            curr=curr.next
-        if l1:
-            curr.next=l1
-        else:
-            curr.next=l2
-        return dh.next
+                prev.next=l2
+            curr=prev.next
+            prev=None
+            while carry!=0 and curr:
+                val1=(curr.val+carry)
+                carry=val1//10
+                curr.val=val1%10
+                prev=curr
+                curr=curr.next
+            if carry!=0:
+                prev.next=ListNode(1)
+            return n1
+    
+
+
+      
