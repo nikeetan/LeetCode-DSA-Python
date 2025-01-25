ce90927f5ebafcbe76075bd2b97b2cdfc038033e
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-01-24 20:31:39 -0600

    Time: 10 ms (12.28%), Space: 18.1 MB (9.67%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,32 +5,39 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        val1,val2=0,0
-        colsum=0
+        n1,n2=0,0
+        sum1=0
+        dh=ListNode(0)
+        curr=dh
         carry=0
-        head=ListNode(0)
-        curr=head
-        while l1 or l2 or carry!=0:
+        while l1 and l2:
+            sum1=(l1.val+l2.val+carry)
+            carry=(sum1//10)
+            newnode=ListNode(sum1%10)
+            curr.next=newnode
+            curr=curr.next
+            l1=l1.next
+            l2=l2.next
+            
+        val1,v1l2=0,0
+        while carry!=0:            
             if l1:
                 val1=l1.val
+                l1=l1.next
             else:
                 val1=0
             if l2:
                 val2=l2.val
-            else:
-                val2=0
-            colsum=val1+val2+carry
-            carry=colsum//10
-            newnode=ListNode(colsum%10)
-            curr.next=newnode
-            curr=newnode
-            if l1:
-                l1=l1.next
-            else:
-                None
-            if l2:
                 l2=l2.next
             else:
-                None
-        return head.next
-
+                val2=0
+            sum1=val1+val2+carry
+            newnode=ListNode(sum1%10)
+            carry=sum1//10
+            curr.next=newnode
+            curr=curr.next
+        if l1:
+            curr.next=l1
+        else:
+            curr.next=l2
+        return dh.next
