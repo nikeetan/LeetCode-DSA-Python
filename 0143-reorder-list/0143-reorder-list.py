03fd6479709f3291ebd60f1f6cdec5ce412a2ea5
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-01-29 10:24:42 -0600

    Time: 0 ms (100%), Space: 23.2 MB (60.32%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -4,44 +4,26 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverse(self,head):
-        prev = None
-        curr = head
-        while curr is not None:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
-        return prev
-
-    def find_middle(self,head):
-        slow = head
-        fast = head
-        while fast is not None and fast.next is not None:
-            slow = slow.next
-            fast = fast.next.next
-        return slow
-
+    '''
+    find middle
+    '''
     def reorderList(self,head):
-        if head is None or head.next is None:
-            return head
+        slow,fast=head,head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
 
-       
-        middle = self.find_middle(head)
-
-        
-        second_half = self.reverse(middle.next)
-        middle.next = None 
-
-        first_half = head
-        while second_half is not None:
-            temp1 = first_half.next
-            temp2 = second_half.next
-
-            first_half.next = second_half
-            second_half.next = temp1
-
-            first_half = temp1
-            second_half = temp2
-
+        prev=None
+        curr=slow
+        while curr:
+            curr.next,prev,curr=prev,curr,curr.next
+        first,second=head,prev
+        while second.next:
+            first.next,first=second,first.next
+            second.next,second=first,second.next
         return head
+    
+
+
+
+    
