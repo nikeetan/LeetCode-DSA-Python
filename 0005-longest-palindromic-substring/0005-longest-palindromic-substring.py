81e00c7085b87791cd41b6fef32542ab88f4d5e9
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-01-29 20:02:35 -0600

    Time: 6695 ms (8.66%), Space: 18.2 MB (20.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,10 +1,6 @@
 class Solution:
    
     def longestPalindrome(self, s: str) -> str:
-        '''
-        intialize two pointer
-        generate the substring
-        '''
         ind={}
         for i in range(len(s)):
             if s[i] not in ind:
@@ -16,29 +12,32 @@ class Solution:
         else:
             to_return=""
             for i in range(len(s)):
-                for j in ind[s[i]]:
-                    p1=i
-                    p2=-1
-                    flag=0
-                    if j!=p1:
-                        p2=j
-                    while s[p1]!=s[p2]:
-                        p2-=1
-                    if p1<p2:
-                        cp1=p1
-                        cp2=p2
-                        while p1<p2:
-                            if s[p1]!=s[p2]:
-                                flag=1
-                                break
-                            else:
-                                p1+=1
-                                p2-=1
-                        if ((flag==0) and (len(to_return)<cp2-cp1+1)):
-                            to_return=s[cp1:cp2+1]
+                if len(ind[s[i]])==1:
+                    continue
+                else:
+                    for j in ind[s[i]]:
+                        p1=i
+                        p2=-1
+                        flag=0
+                        if j!=p1:
+                            p2=j
+                        while s[p1]!=s[p2]:
+                            p2-=1
+                        if p1<p2:
+                            cp1=p1
+                            cp2=p2
+                            while p1<p2:
+                                if s[p1]!=s[p2]:
+                                    flag=1
+                                    break
+                                else:
+                                    p1+=1
+                                    p2-=1
+                            if ((flag==0) and (len(to_return)<cp2-cp1+1)):
+                                to_return=s[cp1:cp2+1]
             if len(to_return)==0:
                 return s[0]
             else:
                 return to_return
 
-                        
+                            
