fb677c77f3748a6c8f1cd83899135bf4653610fa
Niketan Doddamani <niketandoddamani@gmail.com>
2025-02-01 16:14:37 -0600

    Added Infix to Postfix

---
+++ Module2/Infixtopostfix.py
@@ -0,0 +1,34 @@
+def fetch_p(s:str):
+
+    if s=='^':
+        return 3
+    elif s=='*' or s=='/':
+        return 2
+    elif s=='+' or s=="-":
+        return 1
+    else:
+        return -1
+    
+def infix_to_postfix(stack:list[str],exp)->str:
+    to_return=""
+    for i in exp:
+        if (i>='0' and i <'9') or (i>='a' and i<='z') or (i>='A' and i<='Z'):
+            to_return+=i
+        elif i=='(':
+            stack.append(i)
+        elif i==')':
+            while stack[-1]!='(':
+                to_return+=stack.pop()
+            stack.pop()
+        else:   
+            while stack and (fetch_p(i)<=fetch_p(stack[-1])):
+                to_return+=stack.pop()
+            stack.append(i)
+    while stack:
+        to_return+=stack.pop()        
+    return to_return
+
+
+stack=[]
+exp="a+b*(c^d-e)^(f+g*h)-i"
+print(infix_to_postfix(stack,exp))
