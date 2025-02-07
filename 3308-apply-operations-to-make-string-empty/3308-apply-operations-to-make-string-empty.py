8d6832422fbc9e863828eb5b7bd6b3a61ffc5cab
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-07 14:21:57 -0600

    Time: 968 ms (5.41%), Space: 20.3 MB (84.32%) - LeetHub

--- 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.py
+++ 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.py
@@ -1,5 +1,25 @@
 class Solution:
     def lastNonEmptyString(self, s: str) -> str:
+        d={}
+        maxi=float('-inf')
+        for i in s:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+            maxi=max(maxi,d[i])
+        d={}
+        k=""
+        for i in s:
+            if i not in d:
+                d[i]=1
+            else:
+                d[i]+=1
+            if d[i]==maxi:
+                k+=i
+        return k
+            
+        '''
 
         d={}
         for i in range(len(s)):
@@ -16,3 +36,4 @@ class Solution:
                 to_return.append(values[-1])
         to_return=sorted(to_return)
         return "".join(s[i] for i in to_return)
+        '''
