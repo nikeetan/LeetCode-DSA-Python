0b74a0a0be97b2f04da74b38b7f9c99b5029ab7c
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-07 14:22:25 -0600

    Time: 962 ms (5.41%), Space: 20.3 MB (91.89%) - LeetHub

--- 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.py
+++ 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.py
@@ -19,21 +19,3 @@ class Solution:
                 k+=i
         return k
             
-        '''
-
-        d={}
-        for i in range(len(s)):
-            if s[i] not in d:
-                d[s[i]]=[i]
-            else:
-                d[s[i]]+=[i]
-        maxi=float('-inf')
-        for key,values in d.items():
-            maxi=max(len(values),maxi)
-        to_return=[]
-        for key,values in d.items():
-            if len(values)==maxi:
-                to_return.append(values[-1])
-        to_return=sorted(to_return)
-        return "".join(s[i] for i in to_return)
-        '''
