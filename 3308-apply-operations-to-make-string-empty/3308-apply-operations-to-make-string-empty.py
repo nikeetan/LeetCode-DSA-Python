7448bb73ccd04a5d036eff4e9083b48bbf40018a
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-07 14:22:48 -0600

    Time: 633 ms (15.68%), Space: 41.5 MB (5.95%) - LeetHub

--- 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.py
+++ 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.py
@@ -1,21 +1,17 @@
 class Solution:
-    def lastNonEmptyString(self, s: str) -> str:
+    def lastNonEmptyString(self, s: str) -> str:            
         d={}
+        for i in range(len(s)):
+            if s[i] not in d:
+                d[s[i]]=[i]
+            else:
+                d[s[i]]+=[i]
         maxi=float('-inf')
-        for i in s:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]+=1
-            maxi=max(maxi,d[i])
-        d={}
-        k=""
-        for i in s:
-            if i not in d:
-                d[i]=1
-            else:
-                d[i]+=1
-            if d[i]==maxi:
-                k+=i
-        return k
-            
+        for key,values in d.items():
+            maxi=max(len(values),maxi)
+        to_return=[]
+        for key,values in d.items():
+            if len(values)==maxi:
+                to_return.append(values[-1])
+        to_return=sorted(to_return)
+        return "".join(s[i] for i in to_return)
