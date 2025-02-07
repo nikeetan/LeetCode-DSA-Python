eea40197cb1246fa40075860b45b4869314e8220
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-07 14:01:50 -0600

    Time: 643 ms (15.14%), Space: 41.4 MB (5.95%) - LeetHub

--- 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.py
+++ 3308-apply-operations-to-make-string-empty/3308-apply-operations-to-make-string-empty.py
@@ -1,23 +1,26 @@
 class Solution:
     def lastNonEmptyString(self, s: str) -> str:
-        d={}
-        for i in range(len(s)):
-            if s[i] not in d:
-                d[s[i]]=[i]
-            else:
-                d[s[i]]+=[i]
-        maxi=float('-inf')
-        for key,values in d.items():
-            maxi=max(len(values),maxi)
-        to_return=[]
-        for key,values in d.items():
-            if len(values)==maxi:
-                to_return.append(values[-1])
-        to_return=sorted(to_return)
-        k=""
-        for i in to_return:
-            if len(k)==0:
-                k=s[i]
-            else:
-                k+=s[i]
-        return k
+        if len(set(s))==len(s):
+            return s
+        else:
+            d={}
+            for i in range(len(s)):
+                if s[i] not in d:
+                    d[s[i]]=[i]
+                else:
+                    d[s[i]]+=[i]
+            maxi=float('-inf')
+            for key,values in d.items():
+                maxi=max(len(values),maxi)
+            to_return=[]
+            for key,values in d.items():
+                if len(values)==maxi:
+                    to_return.append(values[-1])
+            to_return=sorted(to_return)
+            k=""
+            for i in to_return:
+                if len(k)==0:
+                    k=s[i]
+                else:
+                    k+=s[i]
+            return k
