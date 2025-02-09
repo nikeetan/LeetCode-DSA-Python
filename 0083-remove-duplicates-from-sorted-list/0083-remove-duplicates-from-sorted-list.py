0f6ce1f8027ace57e467f754a1feb04005dafa5e
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-09 14:09:15 -0600

    Time: 0 ms (100%), Space: 18 MB (19.14%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -5,24 +5,14 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        '''
-        Gareeb approach
-        take in hash and remove the duplicate values
-        '''
         if head:
-            temp=head
-            d={}
-            new_list=ListNode(0)
-            head=new_list
-            prev=None
-            while temp is not None:
-                if temp.val not in d:
-                    d[temp.val]=1
-                    new_list.val=temp.val
-                    new_list.next=ListNode(0,None)
-                    prev=new_list
-                    new_list=new_list.next
-                temp=temp.next
-            prev.next=None
+            curr,curr_next=head,head.next
+            while curr_next:
+                while curr_next and curr_next.val==curr.val:
+                    curr_next=curr_next.next
+                curr.next=curr_next
+                curr=curr_next
+                if curr_next:
+                    curr_next=curr_next.next
         return head
         
