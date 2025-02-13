f9e4e74bdc2bff2b16ea2fd74293f8cdff5026f1
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-13 10:11:09 -0600

    Time: 29 ms (95.67%), Space: 17.6 MB (85.63%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -5,7 +5,6 @@ class Solution:
             dp[0]=0
             dp[1]=1
             for i in range(2,n+1):
-                print(i,i-1,i-2)
                 dp[i]=dp[i-1]+dp[i-2]
             return dp[n]
         else:
