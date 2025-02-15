5ab277a473fec03d5df7a742459842cd6b8baa9a
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-14 18:37:26 -0600

    Time: 45 ms (36.74%), Space: 32.1 MB (15.4%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
@@ -1,24 +1,25 @@
 class ProductOfNumbers:
     def __init__(self):
         self.nums=[]
+        self.index=-1
     def add(self, num: int) -> None:
-        if len(self.nums)>=1:
-            if self.nums[-1]==0:
-                num*=1
-            else:
+        if num==0:
+            self.index=len(self.nums)
+            self.nums.append(1)
+        else:
+            if self.nums:
                 num*=self.nums[-1]
-        self.nums.append(num)
+            self.nums.append(num)
     def getProduct(self, k: int) -> int:
+       
         if len(self.nums)==k:
-            if 0 in self.nums:
+            if self.index>=0:
                 return 0
             return self.nums[-1]
-        else:
-            if 0 in self.nums[-(k):]:
-                return 0
-            elif self.nums[-k-1]==0:
-                return self.nums[-1]
-            return self.nums[-1]//self.nums[-k-1]
+        elif self.index<=len(self.nums)-k-1:
+            return self.nums[-1]//self.nums[len(self.nums)-k-1]
+        return 0
+        
         
 
 
