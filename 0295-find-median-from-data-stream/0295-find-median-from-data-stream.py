39ee420ec6e99d981068e4fbb3c8ce9eba630f03
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-16 11:58:23 -0600

    Time: 834 ms (8.37%), Space: 39.2 MB (87.21%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -2,22 +2,47 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
+        '''
         self.heap1=[]
         self.heap2=[]
+        '''
+        self.l1=[]
     
     def addNum(self, num: int) -> None:
+        if len(self.l1)>0:
+            l,r=0,len(self.l1)-1
+            while l<=r:
+                mid=l+(r-l)//2
+                if num<self.l1[mid]:
+                    r=mid-1
+                else:
+                    l=mid+1
+            self.l1.insert(l,num)
+        else:
+            self.l1.insert(0,num)
+
+        '''
         heappush(self.heap1,-num)
         heappush(self.heap2,-heappop(self.heap1))
         if len(self.heap2)>len(self.heap1):
             heappush(self.heap1,-heappop(self.heap2))
-        
+        '''
     def findMedian(self) -> float:
+        if len(self.l1)%2==0:
+            middle=len(self.l1)//2
+            median=(self.l1[middle-1]+self.l1[middle])/2
+        else:
+            median=self.l1[(len(self.l1)//2)]
+        return median        
+
+
+        '''
 
         if len(self.heap1)>len(self.heap2):
             return -self.heap1[0]
         else:
             return (-self.heap1[0]+self.heap2[0])/2
-
+        '''
 
 
 
