9f35d8c8665c5effb35ee641b26aa118cc11242a
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-17 17:45:55 -0600

    Time: 77 ms (14.98%), Space: 19.5 MB (30.23%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -1,15 +1,14 @@
 class Solution:
-    '''
+    
     def helper(self,tiles,bl,sl,index):
-        if index==len(tiles):
-            if sl:
-                bl.add(''.join(sl))
-            return
+        if sl:
+            bl.add(''.join(sl))
         for i in range(index,len(tiles)):
             tiles[i],tiles[index]=tiles[index],tiles[i]
             self.helper(tiles,bl,sl+[tiles[index]],index+1)
-            self.helper(tiles,bl,sl,index+1)
             tiles[i],tiles[index]=tiles[index],tiles[i]
+        
+
     '''     
     def helper(self,char_count): 
         total_cnt=0
@@ -21,14 +20,27 @@ class Solution:
             total_cnt+=self.helper(char_count)
             char_count[i]+=1
         return total_cnt  
+    '''
 
 
     def numTilePossibilities(self, tiles: str) -> int:
+        tiles=list(tiles)
+        bl=set()
+        sl=[]
+        index=0
+        self.helper(tiles,bl,sl,index)
+        print(bl)
+        return len(bl)
+
+
+
+        '''
         char_count=[0]*26
         total_cnt=0
         for i in tiles:
             char_count[(ord(i)-ord('A'))]+=1
         return self.helper(char_count)
+        '''
 
 
         
