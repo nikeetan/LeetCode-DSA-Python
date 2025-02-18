9fba267d42228078f93d3003f009c5c54ec73647
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-17 19:59:03 -0600

    Time: 0 ms (100%), Space: 17.8 MB (49.01%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.py
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.py
@@ -1,25 +1,23 @@
 class Solution:
     def maxAscendingSum(self, nums: List[int]) -> int:
-        '''
-        stack=[]
-        ans=0
-        for i in nums:
-            while stack and stack[-1]>=i:
-                ans=max(ans,sum(stack))
-                stack=[]
-            stack.append(i)
-        if stack:
-            ans=max(ans,sum(stack))
-        return ans
-        '''
-        p1,p2,ascending,ans=0,1,nums[0],0
-        while p2<len(nums):
-            if nums[p2-1]<nums[p2]:
-                ascending+=nums[p2]
-            else:
-                ans=max(ans,ascending)
-                p1=p2
-                ascending=nums[p1]
-            p2+=1
-        ans=max(ans,ascending)
-        return ans
+        if len(nums)==1:
+            return nums[0]
+        else:     
+            p1=0
+            sum1=0
+            maxi=float('-inf')
+            while p1<len(nums)-1:
+                if nums[p1]<nums[p1+1]:
+                    sum1+=nums[p1]
+                else:
+                    sum1+=nums[p1]
+                    maxi=max(maxi,sum1)
+                    sum1=0
+                    p1+=1
+                    continue
+                p1+=1
+            
+            if nums[p1]>nums[p1-1]:
+                sum1+=nums[p1]
+                maxi=max(maxi,sum1)
+            return maxi
