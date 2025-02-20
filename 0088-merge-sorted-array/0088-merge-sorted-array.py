6a008f711df19f3cc683df4eb1b883e82d10ca9f
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-19 21:57:42 -0600

    Time: 0 ms (100%), Space: 17.8 MB (44.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,30 +1,22 @@
 class Solution:
-    def bin_insert(self,nums1,low,high,target):        
-        while low<=high:
-            mid=low+(high-low)//2
-            if nums1[mid]==target:
-                return mid+1
-            elif nums1[mid]>target:
-                high=mid-1
-            else:
-                low=mid+1
-        return low
 
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        while len(nums1)!=m:
-            nums1.pop()
-        for i in nums2:
-            low=0
-            high=len(nums1)-1
-            nums1.insert(self.bin_insert(nums1,low,high,i),i)
-        while len(nums1)>m+n:
-            last=len(nums1)-1
-            while last>=0 and nums1[last]!=0:
-                last-=1
-            nums1.pop(last)
+        p1=m-1
+        p2=n-1
+        k=m+n-1
+        while p1>=0 and p2>=0:
+            if nums1[p1]>nums2[p2]:
+                nums1[k]=nums1[p1]
+                p1-=1
+            else:
+                nums1[k]=nums2[p2]
+                p2-=1
+            k-=1
+        while p2>=0:
+            nums1[k]=nums2[p2]
+            p2-=1
+            k-=1
         return nums1
-        
-            
