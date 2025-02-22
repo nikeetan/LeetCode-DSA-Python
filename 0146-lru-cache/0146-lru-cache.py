c13a1b4659161830907fb4ba2ba59eea80c7fa06
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-22 12:42:37 -0600

    Time: 91 ms (96.79%), Space: 78.2 MB (76.51%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,37 +1,18 @@
-from collections import defaultdict
+from collections import OrderedDict
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.lru=defaultdict(int)
-        self.to_remove=[]
+        self.lru=OrderedDict()
         self.capacity=capacity
-
     def get(self, key: int) -> int:
         if key not in self.lru:
             return -1
         else:
-            self.to_remove.remove(key)
-            self.to_remove.insert(0,key)
+            self.lru.move_to_end(key)
             return self.lru[key]
-
-
     def put(self, key: int, value: int) -> None:
-       
-        if len(self.lru)==self.capacity and key not in self.lru:
-            k=self.to_remove.pop()
-            del(self.lru[k])
-        elif key in self.to_remove:
-            self.to_remove.remove(key)
-        self.to_remove.insert(0,key)
         self.lru[key]=value
-
-
-
-        
-
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+        self.lru.move_to_end(key)
+        if len(self.lru)>self.capacity:
+            self.lru.popitem(last=False)
