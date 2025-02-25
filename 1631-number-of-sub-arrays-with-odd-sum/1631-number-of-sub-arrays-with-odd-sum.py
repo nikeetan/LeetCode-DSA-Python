87ed3d02ebc8e07c18f028c389926f1c7fe9d6b9
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-24 20:33:29 -0600

    Time: 53 ms (94.62%), Space: 21.9 MB (66.67%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
@@ -3,11 +3,12 @@ class Solution:
         os=0
         es=0
         sum1=0
+        k=len(arr)
         for i in arr:
             sum1+=i
             if sum1%2==0:
                 es+=1
             else:
                 os+=1
-        return (os*(es+1))%(pow(10,9)+7)
+        return ((k-es)*(es+1))%(pow(10,9)+7)
 
