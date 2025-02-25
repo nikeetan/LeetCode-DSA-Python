35358ca03e431bb647fb69662db3988391270973
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-24 20:31:22 -0600

    Time: 62 ms (86.02%), Space: 21.8 MB (66.67%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
@@ -3,12 +3,11 @@ class Solution:
         os=0
         es=1
         sum1=0
-        mod = 10**9 + 7
         for i in range(len(arr)):
             sum1+=arr[i]
             if sum1%2==0:
                 es+=1
             else:
                 os+=1
-        return (os*es)%mod
+        return (os*es)%(pow(10,9)+7)
 
