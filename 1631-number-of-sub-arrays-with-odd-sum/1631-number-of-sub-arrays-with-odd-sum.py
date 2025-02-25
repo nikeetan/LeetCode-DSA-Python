0085efdcb06383dc2ec00eba974bcf5c5d90a14f
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-24 20:33:44 -0600

    Time: 61 ms (86.56%), Space: 21.8 MB (66.67%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
-        os=0
         es=0
         sum1=0
         k=len(arr)
@@ -8,7 +7,5 @@ class Solution:
             sum1+=i
             if sum1%2==0:
                 es+=1
-            else:
-                os+=1
         return ((k-es)*(es+1))%(pow(10,9)+7)
 
