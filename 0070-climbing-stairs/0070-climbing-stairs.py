4be535bd19735f5aa9dcea0fdc21ef8f372f3d74
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-02-28 00:16:10 -0600

    Time: 4 ms (1.08%), Space: 17.8 MB (48.8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -7,9 +7,27 @@ class Solution:
     #     for i in steps:
     #         self.helper(steps,n-i,result,sl+[i])
     def climbStairs(self, n: int) -> int:
-        dp=[0]*(n+1)
-        dp[0],dp[1]=1,1
-        for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        print(dp)
+        if n <= 1:
+            return 1
+    
+        dp = [0 for i in range(n+1)]
+        dp[0] = 1
+        dp[1] = 1
+        steps = [1, 2]
+        
+        for i in range(2, n+1):
+            for step in steps:
+                if i - step >= 0:
+                    dp[i] += dp[i-step]  # Add, don't min
+                    print(dp)
+        
         return dp[n]
+        # dp=[float('inf') for i in range(n+1)]
+        # dp[0]=0
+        # coins=[1,2]
+        # for i in range(1,len(dp)):
+        #     for c in coins:
+        #         if i-c>=0:
+        #             dp[i]=min(dp[i],1+dp[i-c])
+        # print(dp)
+        # return dp[n]
