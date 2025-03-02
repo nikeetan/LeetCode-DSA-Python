6ed555c50924415984a78e32d49cfa02c5d831f1
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-03-01 20:42:38 -0600

    Time: 0 ms (100%), Space: 18 MB (62.54%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.py
@@ -1,19 +1,25 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
-        d={}
-        for i in nums1:
-            if i[0] not in d:
-                d[i[0]]=i[1]
+        p1=0
+        p2=0
+        to_return=[]
+        while p1<len(nums1) and p2<len(nums2):
+            if nums1[p1][0]==nums2[p2][0]:
+                to_return.append([nums1[p1][0],nums1[p1][1]+nums2[p2][1]])
+                p1+=1
+                p2+=1
+            elif nums1[p1][0]<nums2[p2][0]:
+                to_return.append(nums1[p1])
+                p1+=1
             else:
-                d[i[0]]+=i[1]
-        for i in nums2:
-            if i[0] not in d:
-                d[i[0]]=i[1]
-            else:
-                d[i[0]]+=i[1]
-        key=sorted(d)
-        to_return=[[i,d[i]] for i in key]
+                to_return.append(nums2[p2])
+                p2+=1
+        if p1==len(nums1):
+            while p2<len(nums2):
+                to_return.append(nums2[p2])
+                p2+=1
+        else:
+            while p1<len(nums1):
+                to_return.append(nums1[p1])
+                p1+=1
         return to_return
-
-
-        
