a72798aaf264c09bf890b9821c074f12d926d96a
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-03-05 12:18:35 -0600

    Time: 50 ms (28.53%), Space: 33.8 MB (94.86%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.py
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.py
@@ -1,25 +1,26 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        to_return=[0]*len(nums)
-        sl=[]
-        l=[]
-        for i in range(len(nums)):
-            if nums[i]<pivot:
-                sl.append(nums[i])
-            elif nums[i]>pivot:
-                l.append(nums[i])
-        no_pivot=len(nums)-(len(sl)+len(l))
-        count_sl,count_l=0,0
-        for i in range(len(to_return)):
-            if sl and count_sl<len(sl):
-                to_return[i]=sl[count_sl]
-                count_sl+=1
+        s,l,e=0,0,0
+        for i in nums:
+            if i<pivot:
+                s+=1
+            elif i>pivot:
+                l+=1
             else:
-                if no_pivot==0:
-                    to_return[i]=l[count_l]
-                    count_l+=1
-                else:
-                    to_return[i]=pivot
-                    no_pivot-=1
-        return to_return
-                
+                e+=1
+        # Pass algo
+        p1=0
+        p2=s
+        p3=s+e
+        ans=[0]*len(nums)
+        for i in nums:
+            if i<pivot:
+                ans[p1]=i
+                p1+=1
+            elif i>pivot:
+                ans[p3]=i
+                p3+=1
+            else:
+                ans[p2]=i
+                p2+=1
+        return ans
