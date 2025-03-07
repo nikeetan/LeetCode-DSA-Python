7c72a59cbed3c64216a667b252b5e35104de6e90
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-03-07 11:13:27 -0600

    Time: 7 ms (86.88%), Space: 17.8 MB (95.31%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.py
@@ -1,7 +1,5 @@
 class Solution:
     '''
-    sieve of eratosthenes
-    '''
     def prime(self,left,right):
         list1=[True for i in range(right+1)]
         list1[0]=list1[1]=False
@@ -31,3 +29,40 @@ class Solution:
             p1+=1
             p2+=1
         return to_return if to_return else [-1,-1]
+    '''
+    def prime(self,num):
+        if num < 2:
+            return False
+        if num == 2 or num == 3:
+            return True
+        if num % 2 == 0:
+            return False    
+        divisor = 3
+        while divisor * divisor <= num:
+            if num % divisor == 0:
+                return False
+            divisor += 2
+        return True
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        prev_prime = -1
+        min_diff = float('inf')
+        to_return = [-1, -1]
+
+        for i in range(left, right + 1):
+            if self.prime(i):
+                if prev_prime != -1:
+                    current_diff = i - prev_prime
+                    if current_diff < min_diff:
+                        min_diff = current_diff
+                        to_return = [prev_prime, i]
+                    if current_diff == 1 or current_diff == 2:
+                        return [prev_prime,i]
+                prev_prime = i
+        return to_return
+
+
+
+
+
+    
+        
