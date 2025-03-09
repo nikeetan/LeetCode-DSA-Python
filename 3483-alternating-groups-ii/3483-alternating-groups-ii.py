6895305bca9cf87dc76562e1f80fb0f2dd8c8e54
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-03-09 11:56:58 -0500

    Time: 721 ms (61.79%), Space: 21.3 MB (57.48%) - LeetHub

--- 3483-alternating-groups-ii/3483-alternating-groups-ii.py
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.py
@@ -1,5 +1,33 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+
+        ### Two passes
+        
+        a_c=1
+        p1,p2=0,1
+        t_c=0
+        while p2<len(colors):
+            if colors[p1]==colors[p2]:
+                a_c=1
+                p1=p2
+                p2=p1+1
+                continue
+            a_c+=1
+            if a_c>=k:
+                t_c+=1
+            p1+=1
+            p2+=1
+        last_ele=colors[-1]
+        for i in range(k-1):
+            if last_ele==colors[i]:
+                break
+            a_c+=1
+            if a_c>=k:
+                t_c+=1
+            last_ele=colors[i]
+        return t_c
+       
+        '''
         colors=colors+colors[0:k-1]
         count=0
         p1,p2=0,1
@@ -11,3 +39,4 @@ class Solution:
                 p1=p1+1
             p2+=1
         return count
+        '''
