d5484c4fb80b9a7a9f52023f78057d56fe1c0d60
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-03-11 22:32:31 -0500

    Time: 0 ms (100%), Space: 18.2 MB (22.96%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.py
@@ -1,19 +1,37 @@
 class Solution:
     def maximumCount(self, nums: List[int]) -> int:
-
-        '''
-        i will use bs to find the first positive index from there till
-        '''
-        pc,nc=0,0
-        p1=0
-        while p1<len(nums):
-            if nums[p1]<0:
-                nc+=1
-            elif nums[p1]>0:
-                pc+=1
+        left,right=0,len(nums)-1
+        ans=-1
+        while left<right:
+            mid=left+(right-left)//2
+            if nums[mid]>0 and nums[mid-1]<=0:
+                ans=mid
+                break
+            elif nums[mid]>0:
+                right=mid
             else:
-                p1+=1
-                continue
-            p1+=1
-        return max(nc,pc)
+                left=mid+1
+        if ans==-1 and nums[left]>0:
+            ans=left
+        left,right=0,len(nums)-1
+        ans1=-1
+        while left<right:
+            mid=right+(left-right)//2
+            if nums[mid]<0 and nums[mid+1]>=0:
+                ans1=mid
+                break
+            elif nums[mid]<0:
+                left=mid+1
+            else:
+                right=mid
+        if ans1==-1 and nums[left]<0:
+            ans1=left
+        if (ans==-1) and (ans1==-1):
+            return 0
+        if ans1==-1:
+            return len(nums)-ans
+        elif ans==-1:
+            return ans1+1
+        else:
+            return max(ans1+1,len(nums)-ans)
         
