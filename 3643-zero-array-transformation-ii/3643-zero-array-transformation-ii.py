84e0774a17e2568339d36b3a5ea02df1f52794d9
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-03-13 06:40:18 -0500

    Time: 157 ms (83.15%), Space: 63.6 MB (40%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -1,5 +1,7 @@
 class Solution:
 
+    '''
+
     def check_all_ele_zero(self,nums:List[int],queries:List[List[int]],queries_len):
 
 
@@ -18,11 +20,30 @@ class Solution:
             if total_sum<nums[i]:
                 return False
         return True
-
+    '''
 
             
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
         # check whether processing all queries i can make 0
+        difference_array=[0]*(len(nums)+1)
+        total=0
+        k=0
+        for i in range(len(nums)):
+            while total+difference_array[i]<nums[i]:
+                k+=1
+                if k>len(queries):
+                    return -1
+                left,right,val=queries[k-1]
+
+                if right>=i:
+                    difference_array[max(left,i)]+=val
+                    difference_array[right+1]-=val
+            total+=difference_array[i]
+        return k 
+
+
+
+        '''
         left=0
         right=len(queries)
         if not self.check_all_ele_zero(nums,queries,right):
@@ -34,6 +55,7 @@ class Solution:
             else:
                 left=mid+1
         return left
+        '''
 
 
 
