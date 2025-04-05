f3cae9b1133e35eaab9282f00b41874b6e948a8e
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-05 11:41:07 -0500

    Time: 4 ms (41.72%), Space: 19.1 MB (11.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,21 +1,3 @@
-'''
-p1, p2 = 0, len(nums) - 1
-while p1 < p2: 
-    current_sum = nums[p1] + nums[p2]
-    if current_sum == target:
-        return [p1, p2]
-    elif current_sum > target:
-        if nums[p1] > nums[p2]:
-            p1 += 1
-        else:
-            p2 -= 1
-    else:
-        if nums[p1] < nums[p2]:
-            p1 += 1
-        else:
-            p2 -= 1
-return -1 
-'''
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
@@ -23,7 +5,8 @@ class Solution:
         for i in range(len(nums)):
             hash_map[nums[i]] = i
         for idx in range(len(nums)):
-            if ((target - nums[idx]) in hash_map) and idx != hash_map[(target - nums[idx])]:
-                return [idx,hash_map[(target - nums[idx])]]
+            complement = target - nums[idx]
+            if ((complement) in hash_map) and idx != hash_map[complement]:
+                return [idx,hash_map[complement]]
         return - 1
         
