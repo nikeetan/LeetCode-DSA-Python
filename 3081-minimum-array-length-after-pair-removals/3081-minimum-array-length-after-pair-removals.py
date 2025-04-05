2039c9a5048b2a91a7fecbb6d764f103218a9038
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-05 13:46:50 -0500

    Time: 67 ms (51.97%), Space: 27.5 MB (93.93%) - LeetHub

---
+++ 3081-minimum-array-length-after-pair-removals/3081-minimum-array-length-after-pair-removals.py
@@ -0,0 +1,13 @@
+import heapq
+from typing import List
+
+class Solution:
+    def minLengthAfterRemovals(self, nums: List[int]) -> int:
+        i, j = 0, (len(nums) + 1)//2
+        ans = len(nums)
+        while ((i < len(nums)//2) and (j< len(nums))):
+            if nums[i] < nums[j]:
+                ans -= 2
+            i += 1
+            j += 1
+        return ans
