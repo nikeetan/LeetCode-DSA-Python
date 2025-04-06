0e8010cdea2069a3edd5f46d0df00b2025d212ee
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-06 18:37:04 -0500

    Time: 0 ms (100%), Space: 17.8 MB (60.73%) - LeetHub

--- 0202-happy-number/0202-happy-number.py
+++ 0202-happy-number/0202-happy-number.py
@@ -1,19 +1,19 @@
 from collections import defaultdict
 class Solution:
-    def isHappy(self, n: int) -> bool:
+    def next_number(self, n):
         new_number = 0
-        hash_numbers = defaultdict(int)
-        while (new_number != 1):
-            while n != 0:
-                remainder = n % 10
-                new_number += remainder * remainder
-                n = n//10
-            if new_number == 1:
-                return True
-            elif new_number in hash_numbers:
-                return False
-            else:
-                hash_numbers [new_number] = 1
-                n = new_number 
-                new_number = 0
-        return True if new_number == 1 else False
+        while n > 0:
+            remainder = n % 10
+            new_number += remainder**2
+            n = n // 10
+        return new_number
+
+    def isHappy(self, n: int) -> bool:
+        slow = n
+        fast = self.next_number(n)
+        while slow != fast and fast != 1:
+            slow = self.next_number(slow)
+            fast = self.next_number(self.next_number(fast))
+
+        return fast == 1
+
