f11b4521e5e8cdfec847a5d8058d086a4aa54338
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-07 19:32:33 -0500

    Time: 0 ms (100%), Space: 17.6 MB (93.16%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,16 +1,14 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        d={}
-        p2=len(nums)-1
-        while p2>=0:
-            if nums[p2] not in d:
-                d[nums[p2]]=1
+        hash_map ={}
+        p1 = len(nums) - 1
+        while p1 >= 0:
+            if nums[p1] not in hash_map:
+                hash_map[nums[p1]] = 1
             else:
-                if (len(nums)-len(d))%3 ==0:
-                    return (len(nums)-len(d))//3
+                if (len(nums) - len(hash_map))%3 ==0:
+                    return (len(nums) - len(hash_map)) // 3
                 else:
-                    return (len(nums)-len(d))//3+1
-            p2-=1
+                    return (len(nums) - len(hash_map)) // 3 + 1
+            p1 -= 1
         return 0
-
-
