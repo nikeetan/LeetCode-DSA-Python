1e110d6f1214e8b6c960e8c58a78ba607fd9e418
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-09 10:35:58 -0500

    Time: 1317 ms (72.68%), Space: 21.2 MB (99.33%) - LeetHub

--- 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
+++ 1713-dot-product-of-two-sparse-vectors/1713-dot-product-of-two-sparse-vectors.py
@@ -1,47 +1,10 @@
-'''
-Is the size of the two matrix same
-
-There might be an empty nums1 while non empty num2 and also vice - versa
-
-The output should be an input
-
-we have to store sparse vector could you provide some more details on this part please
-
-since i should store the sparse vector i am thinking something like a store by index as you said we have both nums1 and nums2 to be of same size we can formulate the store by index
-
-
-if nums1 and (len(nums2) == 0):
-    return 0
-elif nums2 and (len(nums1) == 0):
-    return 0
-
-else:
-// store this in the constructor
-count = 0
-globalself.vec 
-{ 0 : [1, 0],
-  1 : [0, 3],
-  2 : [0, 0],
-  3 : [2, 4],
-  4 : [3, 0]
-}
-
-
-
-// Dot product obtained
-res += numpy.prod(vector[indx])
-return res
-'''
-
 class SparseVector:
     def __init__(self, nums: List[int]):
         self.vec1 = {}
         for i in range(len(nums)):
-            if i not in self.vec1 and nums[i] != 0:
+            if nums[i] != 0:
                 self.vec1[i] = nums[i]
         
-        
-
     # Return the dotProduct of two sparse vectors
     def dotProduct(self, vec: 'SparseVector') -> int:
         if (len(self.vec1) == 0) or (len(vec.vec1) == 0):
