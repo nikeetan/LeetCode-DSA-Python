13a3057555f2fc5e4690c52563f0b8031825ab98
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-09 21:52:11 -0500

    Time: 2 ms (20.38%), Space: 18.4 MB (12.68%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.py
@@ -0,0 +1,27 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        '''
+        DFS
+        '''
+        R, C = len(image), len(image[0])
+        startcolor = image[sr][sc]
+        if startcolor == color:
+            return image
+
+        def dfs (r, c):
+            print(r, c)
+            if image[r][c] == startcolor:
+                image[r][c] = color
+
+                # checking whether we are in second row
+                if r >= 1:
+                    dfs (r - 1, c)
+                if r + 1 < R:
+                    dfs (r + 1, c)
+                if c >= 1:
+                    dfs (r, c - 1)
+                if c + 1 < C:
+                    dfs (r, c + 1)
+
+        dfs (sr, sc)
+        return image
