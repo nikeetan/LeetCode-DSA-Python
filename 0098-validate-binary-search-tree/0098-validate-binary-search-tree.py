aaa9402ec052683628bccd9385115976be8b3ebe
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-10 16:03:59 -0500

    Time: 0 ms (100%), Space: 19.6 MB (90.08%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,31 +6,28 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        '''
-        can i think of bfs here
-        the root might be nOne rigth ?
-        how big is the tree
-        i am thinking of bfs like we will use a queue and check every time checking the node whether the child nodes  will be greater than the parent why i choose bfs because it says we are validating  BST and in BST we have exactly 2 child nodes for a parent node
-        
-
-        root = 5
-        before appending we will check whether the root is > left and < right
-
-        queue = []
-        
-        ele = 3 i dont have left as well as right for 3 
-
-        '''
-        
-
-        def validate(root, low , high):
+        # def validate(root, low , high):
+        #     if root is None:
+        #         return True
+        #     else:
+        #         if (low != None and root.val <= low ) or (high != None and root.val >= high):
+        #             return False
+        #         return validate(root.left, low, root.val)  and validate(root.right, root.val, high)
+        # return validate(root, None, None)
+        prev = float('-inf')
+        def inorder(root):
+            nonlocal prev 
             if root is None:
                 return True
-            else:
-                if (low != None and root.val <= low ) or (high != None and root.val >= high):
-                    return False
-                return validate(root.left, low, root.val)  and validate(root.right, root.val, high)
-        return validate(root, None, None)
-       
+            if not inorder(root.left):
+                return False
+            if root.val <= prev:
+                return False
+            prev = root.val
+            return inorder(root.right)
+        return inorder(root)
+    
+
+
 
         
