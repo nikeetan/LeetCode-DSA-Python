642cd3c956609abdaf042640ffd3c21361b0b232
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-11 07:47:50 -0500

    Time: 7 ms (5.5%), Space: 19 MB (79.28%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -9,16 +9,17 @@ class Solution:
         if root is None:
             return 0
         else:
-            queue = deque([root])
-            depth = 0
-            while queue:
-                level = len(queue)
-                depth += 1
-                for current_level in range(level):
-                    current_node = queue.popleft()
-                    if current_node.left or current_node.right:
-                        if current_node.left:
-                            queue.append(current_node.left)
-                        if current_node.right:
-                            queue.append(current_node.right)   
-            return depth
+            return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
+            # queue = deque([root])
+            # depth = 0
+            # while queue:
+            #     level = len(queue)
+            #     depth += 1
+            #     for current_level in range(level):
+            #         current_node = queue.popleft()
+            #         if current_node.left or current_node.right:
+            #             if current_node.left:
+            #                 queue.append(current_node.left)
+            #             if current_node.right:
+            #                 queue.append(current_node.right)   
+            # return depth
