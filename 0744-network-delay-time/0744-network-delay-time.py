ded88c7830829ace0856d2b34989b682cec1816d
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-12 14:05:02 -0500

    Time: 403 ms (24.54%), Space: 19.9 MB (36.34%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,24 +1,34 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-       # Djikstra's Algorithm
-        graph = defaultdict(list)
-        for u, v, time in times:
-            graph[u].append((v, time))
+        # Adjacency matrix
+        adj = {}
+        for u , v , w in times:
+            if u not in adj:
+                adj[u] = []
+            adj[u].append((w , v))
         
-        min_times = {}
-        min_heap = [(0, k)] # (distance from source to node, node)
+        # Min heap for selection of minimum neighbour node
+        min_heap = [(0, k)]
 
+        # Record the node traversed and time taken
+        visited = {}
+
+        # Beigin the iterative traversal
         while min_heap:
-            time_k_to_i, i = heapq.heappop(min_heap)
-            if i in min_times:
+            time_curr, curr_node = heapq.heappop(min_heap)
+
+            if curr_node in visited:
                 continue
-            
-            min_times[i] = time_k_to_i
-            for nei, nei_time in graph[i]:
-                if nei not in min_times:
-                    heapq.heappush(min_heap, (time_k_to_i + nei_time, nei))
-        
-        if len(min_times) == n:
-            return max(min_times.values())
-        else:
-            return -1
+            visited [curr_node] = time_curr
+
+            for ntime, nnode in adj.get(curr_node, []):
+                print(time_curr + ntime, nnode)
+                if nnode not in visited:
+                    heapq.heappush(min_heap, ((time_curr + ntime), nnode))
+        if len(visited) == n:
+            return max(visited.values())
+        return -1
+
+                    
+
+
