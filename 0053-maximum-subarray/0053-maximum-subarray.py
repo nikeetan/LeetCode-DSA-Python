4640414d5f7262a75af7de8a57ce8e17f06803b3
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-11 20:54:10 -0500

    Time: 98 ms (13.28%), Space: 32 MB (83.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,10 +1,29 @@
+'''
+maxi = 0
+p1 = 0 
+p2 = 0
+current_sum = 0                                         [5, 4 , -1 , 7, 8]
+for p1 in range(len(nums)):         o(n)                                p1
+    for p2 in range(p1,len(nums)):  o(n)                 23 23  23  23  23
+        current_sum += nums[p2]                          # 
+        maxi = max(maxi, current_sum) tc : o(n**2), sc : o(1)
+return maxi
+
+'''
+
+'''                          [-2,1,-3,4,-1,2,1,-5,4]
+ans = float('-inf')          ans = - Null
+curr_sum = 0                    
+for i in range(len(nums)):    
+    curr_sum = max(nums[i], nums[i]+curr_sum
+    ans = max(ans , curr)   
+'''
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        ans=float('-inf')
-        sum1=0
-        for i in nums:
-            sum1+=i
-            ans=max(ans,sum1)
-            if sum1<0:
-                sum1=0
-        return ans
+        ans = float('-inf')          
+        curr_sum = 0                    
+        for i in range(len(nums)):    
+            curr_sum = max(nums[i], nums[i]+curr_sum)
+            ans = max(ans , curr_sum) 
+        return ans  
