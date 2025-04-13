11c1a156be006f576b225f215ecf54296abb43ff
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-13 09:39:43 -0500

    Time: 371 ms (70%), Space: 96.6 MB (78.28%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,32 +1,65 @@
-class Dsu:
-    def __init__(self, n):
-        self.parent = [x for x in range(n)]
+# class Dsu:
+#     def __init__(self, n):
+#         self.parent = [x for x in range(n)]
     
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+#     def find(self, x):
+#         if self.parent[x] != x:
+#             self.parent[x] = self.find(self.parent[x])
+#         return self.parent[x]
 
-    def Union(self, x , y):
-        x = self.find(x)
-        y = self.find(y)
+#     def Union(self, x , y):
+#         x = self.find(x)
+#         y = self.find(y)
 
-        if x != y:
-            self.parent[x] = y
+#         if x != y:
+#             self.parent[x] = y
     
-    def isConnected(self, u, v):
-        return True if self.find(u) == self.find(v) else False 
+#     def isConnected(self, u, v):
+#         return True if self.find(u) == self.find(v) else False 
 
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        check = Dsu(n)
 
-        for u, v in edges:
-            check.Union(u , v)
+        # BFS
+        # adj list 
+        adj = {}
+        for u , v in edges:
+            if u not in adj:
+                adj[u] = []
+            if v not in adj:
+                adj[v] = []
+            adj[u].append(v)                    #{ 0 :[1, 2] , 1:[2], 2:[0]}
+            adj[v].append(u)
+        
+        
+        # queue for traversal
+        queue = deque([source])                 # queue = [0]
 
-        return check.isConnected(source, destination)
+        #visited set    
+        visited = set()                         # []
+                                                # destination = 2
+        while queue:                            # queue = [0] , [1, 2] , [2, 2], [2]
+            node = queue.popleft()              # node = 2 , queue = [2]
+            # Adding the node is visited
+            if node in visited:                 #2 in [0, 1, 2]
+                continue
+            else:
+                visited.add(node)                   # [0, 1, 2]              
+                if node == destination:             # 3 == 2
+                    return True                     # True
+                for neighbour in adj.get(node, []):   #[ 0 ]          
+                    if neighbour not in visited:       # 0 in  [0, 1, 2]
+                        queue.append(neighbour)        # queue = [ 2]
+                    
+        return False
 
+
+
+
+
+
+        
 
 
 
