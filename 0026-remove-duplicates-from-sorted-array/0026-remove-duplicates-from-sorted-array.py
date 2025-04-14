430b1e9e6e26c888edade048832219fe7aa99de7
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-14 08:49:28 -0500

    Time: 3 ms (47.89%), Space: 19.1 MB (9.01%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,10 +1,45 @@
+'''
+fp     sp 
+[0, 0,  1, 1, 1, 1, 2, 2, 3, 3, 4]
+    fp             sp
+[0, 1,  0, 1, 1, 1, 2, 2, 3, 3, 4]
+        fp                sp
+[0, 1,  2, 1, 1, 1, 0, 2, 3, 3, 4]
+             fp                  
+[0, 1,  2, 3, 4, 1, 0, 2, 1, 3, 1]
+
+
+[1, 1, 2]
+if len(nums) <= 1: 
+    return nums
+else:                                 
+    fp , sp = 0 , 1                 
+    while sp < len(nums):
+
+        # not equal condition
+        if nums [fp] != nums[sp]:
+            nums [fp + 1] , nums [sp] = nums [sp], nums [fp]
+            fp = fp + 1
+        sp += 1
+
+TC : o(n)
+SC : o(1)
+
+[1, 2, 1] 
+'''
+
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        secondpointer = 1 
-        unique_count = 0
-        while secondpointer < len(nums):
-            if nums[unique_count] != nums[secondpointer]:
-                unique_count += 1
-                nums[unique_count] = nums[secondpointer]
-            secondpointer += 1
-        return unique_count+1
+        if len(nums) <= 1:
+            return 1
+        else:                                 
+            fp , sp = 0 , 1     #[0 , 1]            
+            while sp < len(nums):
+                # not equal condition
+                if nums [fp] != nums[sp]:       
+                    nums [fp + 1] = nums [sp]
+                    fp = fp + 1
+                sp += 1
+            return fp + 1 
+
