1cde0d364bb22abcb1940cfea898f7a53c4a5a55
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-16 20:35:43 -0500

    Time: 3 ms (76.2%), Space: 18.3 MB (33.17%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,18 @@
+import heapq
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # Creating the adjancency list
+        adj = defaultdict(list)
+        for src , dst in sorted(tickets, reverse = True):
+            adj[src].append(dst)
+            
+        start = 'JFK'
+        #using minheap
+        res = []
+
+        def dfs(node):
+            while adj[node]:
+                dfs(adj[node].pop())
+            res.append(node)
+        dfs(start)
+        return res[::-1]
