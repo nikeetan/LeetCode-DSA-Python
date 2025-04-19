ed398c1953f77073455f95a084872cca5ee4a62f
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-19 11:57:59 -0500

    Time: 155 ms (78.87%), Space: 30.1 MB (87.8%) - LeetHub

--- 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.py
+++ 2439-longest-cycle-in-a-graph/2439-longest-cycle-in-a-graph.py
@@ -1,26 +1,44 @@
 class Solution:
-    def __init__(self):
-        self.ans = - 1
-
-
-    def dfs (self, node, edges, visited, dist):
-        visited [node] = True
-        neighbor = edges[node]
-        
-        if neighbor != -1 and not(visited[neighbor]):
-            dist[neighbor] = dist[node] + 1
-            self.dfs(neighbor, edges, visited, dist)
-        
-        elif neighbor != -1 and neighbor in dist:
-            self.ans = max(self.ans, (dist[node] - dist[neighbor] +1 ))
-
     def longestCycle(self, edges: List[int]) -> int:
-        n = len(edges)
-        visited = [False] * n
-        
-        for i in range(n):
-            if not visited[i]:
-                dist = {i : 1}
-                self.dfs(i, edges, visited, dist)
-        return self.ans
-        
+        # kahns algorithm 
+        indegree  = [0] * len(edges)
+        for i in range(len(edges)):
+            if edges[i] != -1:
+                indegree[edges[i]] += 1
+
+        # intializing the queue
+        queue = deque()
+        # adding the nodes whose indegree are 0
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+        visited = [False] * len(edges)
+        # reaching to the cycle
+        while queue:
+            node = queue.popleft()
+            visited[node] = True
+            nei = edges[node]
+            if nei != -1:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+        #lets check visited
+
+        if all(visited):
+            return -1
+        else:
+            # start with the first unvisited node
+            ans = -1
+            for i in range(len(edges)):
+                nei = edges[i]
+                if not(visited[i]):
+                    cnt = 1
+                    visited[i] = True
+                    while nei != i:
+                        visited[nei] = True
+                        cnt += 1
+                        nei = edges[nei]
+                    ans = max(ans, cnt)
+        return ans
+
+    
