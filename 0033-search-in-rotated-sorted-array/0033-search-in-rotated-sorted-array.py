d69ff2d6f5ba2485063b0f1d81075fbe43c7b150
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-20 13:12:29 -0500

    Time: 0 ms (100%), Space: 18.3 MB (22.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,21 +1,45 @@
+'''
+[4,5,6,7,0,1,2] target = 3
+itr 1
+l, r = 0, 6
+mid = 3
+if target == mid:
+    return mid
+elif target < mid and target >= lt:
+    right = mid - 1
+else:
+    left = mid + 1
+return - 1
+
+
+itr 2
+l, r = 4, 6
+mid = 5
+
+r = 4
+
+itr 3
+l,r = 4 + 4
+mid = 4
+4
+'''
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        '''
-        primary approach
-        '''
-        low,high=0,len(nums)-1
-        while low<=high:
-            mid=low+(high-low)//2
-            if nums[mid]==target:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            mid = l + (r - l)//2
+            print(mid, l , r)
+            if target == nums[mid]:
                 return mid
-            if nums[low]<=nums[mid]:
-                if nums[low]<=target and target<nums[mid]:
-                    high=mid-1
+            if nums[l] <= nums[mid]:
+                if target >= nums[l] and target < nums[mid]:
+                    r = mid - 1
                 else:
-                    low=mid+1
+                    l = mid + 1
             else:
-                if nums[mid]<target and target<=nums[high]:
-                    low=mid+1
+                if target > nums[mid] and target <= nums[r]:
+                   l = mid + 1
                 else:
-                    high=mid-1
+                    r = mid - 1
         return -1
