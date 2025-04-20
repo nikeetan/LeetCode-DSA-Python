8c68fc9896bf8a41f5cc6b2a3f0d0839b51188a7
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-20 11:16:23 -0500

    Time: 5 ms (10.86%), Space: 19.9 MB (20.32%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -27,24 +27,53 @@ class Solution:
 
         current interval starts after the new interval ends
         '''
-        indx = 0
+        # indx = 0
+        # res = []
+        # # Finding overlapping interval
+        # while ((indx < len(intervals)) and (intervals[indx][1] < newInterval[0])):
+        #     res.append(intervals[indx])
+        #     indx += 1
+        
+        # #Merging
+        # while indx < len(intervals) and newInterval[1] >= intervals[indx][0]:
+        #     # minimum of start
+        #     newInterval[0] = min(intervals[indx][0], newInterval[0])
+        #     # maximum of end
+        #     newInterval[1] = max(intervals[indx][1], newInterval[1])
+        #     indx += 1
+        # res.append(newInterval)
+        
+        # # adding the remaining:
+        # while indx < len(intervals):
+        #     res.append(intervals[indx])
+        #     indx += 1
+        # return res
+
         res = []
-        # Finding overlapping interval
-        while ((indx < len(intervals)) and (intervals[indx][1] < newInterval[0])):
-            res.append(intervals[indx])
-            indx += 1
+        # first we wil find the position to insert the new interval through bs why bs bcz iuntervals are sorted
+        left, right = 0 , len(intervals) - 1
+        while left <= right:
+            mid = left + (right - left)//2
+            if intervals[mid][0] > newInterval[0]:
+                right = mid - 1
+            else:
+                left = mid + 1
         
-        #Merging
-        while indx < len(intervals) and newInterval[1] >= intervals[indx][0]:
-            # minimum of start
-            newInterval[0] = min(intervals[indx][0], newInterval[0])
-            # maximum of end
-            newInterval[1] = max(intervals[indx][1], newInterval[1])
-            indx += 1
-        res.append(newInterval)
-        
-        # adding the remaining:
-        while indx < len(intervals):
-            res.append(intervals[indx])
-            indx += 1
+        # check bs works and needs any improvements
+        intervals.insert(left, newInterval)
+        print(intervals)
+        # now adding one by one
+        for interval in intervals:
+            
+            if len(res) == 0:
+                res.append(interval)
+            elif res[-1][-1] >= interval[0]:
+                res[-1][0] = min(res[-1][0], interval[0])
+                res[-1][1] = max(res[-1][1], interval[1])
+            else:
+                res.append(interval)
         return res
+            
+
+
+        return []
