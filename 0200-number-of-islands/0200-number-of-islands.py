3d33f8bc1c6bc374ca7c05558ca8c9ec7ea47aa0
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-21 11:04:15 -0500

    Time: 233 ms (86.66%), Space: 20.4 MB (39.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,6 +1,21 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+
+        def dfs(curr_row, curr_col):
+            grid[curr_row][curr_col] = "0"
+            for dirX, dirY in directions:
+                nei_row = curr_row + dirX
+                nei_col = curr_col + dirY
+                
+                if ((0 <= nei_row < R) and(0 <= nei_col < C) and (grid[nei_row][nei_col] != "0")):
+                        dfs(nei_row, nei_col)
+                        #mark visited
+                       
+
+
+
+
         def bfs(curr_row, curr_col):
             grid[curr_row][curr_col] = '0'
             queue = deque([(curr_row, curr_col)])
@@ -25,7 +40,8 @@ class Solution:
             for j in range(C):
                 if grid[i][j] == '1':
                     no_of_island += 1
-                    bfs(i, j)
+                    #bfs(i, j)
+                    dfs(i, j)
         return no_of_island
 
         
