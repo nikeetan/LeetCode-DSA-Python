c3a0b578288bc70a0dab62f8fbda05a2fe12c473
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-21 16:10:05 -0500

    Time: 11 ms (63.52%), Space: 20.3 MB (52.68%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -3,27 +3,26 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+'''
+heapq solution = [(val, node_head)]
+'''
+import heapq
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        stack=[]
-        for i in lists:
-            temp=i  
-            while temp:
-                stack.append(temp.val)
-                temp=temp.next     
-        if len(stack)==1:
-            newnode=ListNode(stack[0])
-            return newnode
-        elif len(stack)>1:
-            stack=sorted(stack)
-            newnode=ListNode(0)
-            head=newnode
-            for i in range(len(stack)-1):
-                newnode.val=stack[i]
-                newnode.next=ListNode(0,None)
-                newnode=newnode.next
-            newnode.val=stack[i+1]
-            return head
-        else:
-            return None
-        
+        queue = []
+        for i , curr_list in enumerate(lists):
+            if curr_list:
+                heapq.heappush(queue , (curr_list.val, i, curr_list))
+
+        dummy = ListNode(0)
+        temp = dummy
+
+        while queue:
+            value, indx,  curr_node = heapq.heappop(queue)
+            temp.next = ListNode(value)
+            temp = temp.next
+            if curr_node.next:
+                curr_node = curr_node.next
+                heapq.heappush(queue , (curr_node.val, indx, curr_node))
+        return dummy.next
