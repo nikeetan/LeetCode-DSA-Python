863cae2da55025e777e865c0323ebbc041b90917
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-22 13:33:30 -0500

    Time: 23 ms (49.23%), Space: 17.9 MB (82.53%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,25 +1,15 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransom_map = {}
-        magazine_map = {}
+        word_count = [0] * 26
+        unique_chars = set(ransomNote)
         for i in ransomNote:
-            if i not in ransom_map:
-                ransom_map[i] = 1
-            else:
-                ransom_map[i] += 1
+            word_count[ord(i) - ord('a')] += 1
+
         for i in magazine:
-            if i not in magazine_map:
-                magazine_map[i] = 1
-            else:
-                magazine_map[i] += 1
-        compare_keys = list(ransom_map.keys())
-        for i in compare_keys:
-            if i not in magazine_map:
-                return False
-            elif ransom_map[i] > magazine_map[i]:
-                return False
-        return True
-            
-
-
+            word_count[ord(i) - ord('a')] -= 1
         
+        for i in unique_chars:
+            if word_count[ord(i) - ord('a')] > 0:
+                return False
+        print(word_count)
+        return True
