cd0a7707e5581c4f90e76d0775faa5228ad864c3
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-23 11:05:08 -0500

    Time: 156 ms (23.78%), Space: 41.9 MB (30.12%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.py
@@ -1,24 +1,64 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.flag = False
+
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        folder_set = set(folder)
+        head = TrieNode()
+        for curr_dir in folder:
+            node = head
+            folder = curr_dir.split("/")[1:]
+            for sub_folder in folder:
+                if sub_folder not in node.children:
+                    node.children[sub_folder] = TrieNode()
+                node = node.children[sub_folder]
+            node.flag = True
+
         res = []
-
-        for f in folder:
-            is_sub = False
-            prefix = f
-
-            while prefix and (not(is_sub)):
-                
-                print("Before",prefix)
-                pos = prefix.rfind("/")
-                
-                prefix = prefix[0 : pos]
-                print("After",prefix)
-    
-                if prefix in folder_set:
-                    is_sub = True
-
-            if is_sub == False:
-                res.append(f)
-            
+        path = []
+        def dfs(node, path):
+            if node.flag:
+                res.append("/" + "/".join(path))
+                return
+            for sub_folder, next_folder in node.children.items():
+                dfs(next_folder, path + [sub_folder])
+        dfs(head, [])
         return res
+
+
+            
+
+
+
+
+
+
+        # head = TrieNode()
+    
+        # for path in folder:
+        #     node = head
+        #     paths = path.split("/")[1:]
+        #     print(paths)
+        #     for part in paths:
+        #         if part not in node.children:
+        #             node.children[part] = TrieNode()
+        #         node = node.children[part]
+        #     node.flag = True
+        
+        # res = []
+        # def dfs(root, folder):
+        #     print(folder)
+        #     if root.flag:
+        #         res.append('/' + '/'.join(folder))
+        #         return
+        #     for path, child in root.children.items():
+        #         print("First",path)
+        #         dfs(child, folder + [path])    
+        # dfs(head, [])
+        # return res
+
+
+
+
+                
