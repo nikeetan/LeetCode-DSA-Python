bea670cc8331e745b903b273f1fd84c1430980b7
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-25 23:04:13 -0500

    Time: 132 ms (66.22%), Space: 78.7 MB (13.44%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -17,60 +17,96 @@ and when i insert i insert at the 0th position so put takes o(1)
 '''
 
 
-class OrderedDict:
-    def __init__(self):
-        self.store = {}
-        self.order = []
+# class OrderedDict:
+#     def __init__(self):
+#         self.store = {}
+#         self.order = []
     
-    def move_to_end(self, key):
-        if key not in self.order:
-            return -1
-        else:
-            self.order.remove(key)
-            self.order.append(key)
-    def popitem(self, last):
-        if last:
-            key = self.order.pop()
-        else:
-            key = self.order.pop(0)
-        self.store.pop(key)
+#     def move_to_end(self, key):
+#         if key not in self.order:
+#             return -1
+#         else:
+#             self.order.remove(key)
+#             self.order.append(key)
+            
+#     def popitem(self, last):
+#         if last:
+#             key = self.order.pop()
+#         else:
+#             key = self.order.pop(0)
+#         self.store.pop(key)
     
-    def __len__ (self):
-        return len(self.store)
+#     def __len__ (self):
+#         return len(self.store)
     
-    def __contains__ (self, key):
-        return key in self.store
+#     def __contains__ (self, key):
+#         return key in self.store
 
-    def __getitem__(self, key):
-        if key in self.store:
-            return self.store[key]
-        else:
-            return -1
+#     def __getitem__(self, key):
+#         if key in self.store:
+#             return self.store[key]
+#         else:
+#             return -1
     
-    def __setitem__(self, key, value):
-        if key not in self.store:
-            self.order.append(key)
-        self.store[key] = value
+#     def __setitem__(self, key, value):
+#         if key not in self.store:
+#             self.order.append(key)
+#         self.store[key] = value
 
+
+class ListNode:
+
+    def __init__(self, key, val):
+        self.next = None
+        self.prev = None
+        self.val = val
+        self.key = key 
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.lru = OrderedDict()
         self.capacity = capacity
-    
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.dic = {}
 
     def get(self, key: int) -> int:
-        if key in self.lru:
-            self.lru.move_to_end(key)
-            return self.lru[key]
-        else:
+        if key not in self.dic:
             return -1
+        else:
+            node = self.dic[key]
+            self.remove(node)
+            self.add(node)
+            return node.val
+            
     def put(self, key: int, value: int) -> None:
-        self.lru[key] = value
-        self.lru.move_to_end(key)
-        if len(self.lru) > self.capacity:
-            self.lru.popitem(last = False)
+        if key in self.dic:
+            old_node = self.dic[key]
+            self.remove(old_node)
+
+        node = ListNode(key, value)
+        self.dic[key] = node
+        self.add(node)
+
+        if len(self.dic) > self.capacity:
+            del_node = self.head.next
+            self.remove(del_node)
+            del self.dic[del_node.key]
+
+    def add(self, node):
+        previous_node = self.tail.prev
+        previous_node.next = node
+        node.prev = previous_node
+        node.next = self.tail
+        self.tail.prev = node
+       
+
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
         
 
 
