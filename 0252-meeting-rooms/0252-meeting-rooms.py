a41a5c6b828588e67d82105bf4336fd4eceaf74f
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-28 10:02:55 -0500

    Time: 3 ms (76.77%), Space: 19.7 MB (83.87%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -3,17 +3,10 @@ class Solution:
         
         intervals.sort()
         if len(intervals) > 1:
-            prev_end = intervals[0][-1]
-            for i in range(1, len(intervals)): 
-                if intervals[i][0] < prev_end:
+            for i in range(0, len(intervals) - 1): 
+                if intervals[i][1] > intervals[i + 1][0]:
                     return False
-                prev_end = intervals[i][-1]
             return True
         else:
             return True
-        # Time complexity is 
-        # O(n log n) for sorting
-        # and for traversing O (n)
-        # so total will be o(n logn) + o(n)
-        # since o(n logn ) will be dominant my total time complexity would be o(n logn)
-        # Space complexity = O(1) no extra space is being used
+       
