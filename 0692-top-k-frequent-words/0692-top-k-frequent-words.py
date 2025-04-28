e86c2c33256705ad22017857535467cf863fb13c
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-28 18:48:38 -0500

    Time: 11 ms (5.87%), Space: 17.9 MB (39.82%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,18 +1,43 @@
+from collections import Counter
+from heapq import heappush, heappop
+
+class TrieNode:
+    def __init__(self):
+        self.child = {}
+        self.flag = False
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        freq_map = {}
-        word_heap = []
-        for i in words:
-            if i not in freq_map:
-                freq_map[i] = 1
-            else:
-                freq_map[i] += 1
+        cnt = Counter(words)
+        w_bucket = [ None for i in range(len(words) + 1)]
+        for word, freq in cnt.items():
+            if not w_bucket[freq]:
+                w_bucket[freq] = TrieNode()
+            
+            node = w_bucket[freq]
+            for i in word:
+                if i not in node.child:
+                    node.child[i] = TrieNode()
+                node = node.child[i]
+            node.flag = True
 
-        for key, val in freq_map.items():
-            heapq.heappush(word_heap, (- val, key))
         res = []
-        while k:
-            res.append(heapq.heappop(word_heap)[-1])
-            k -= 1
-        return res
+
+        def dfs(node, word):
+            if node.flag:
+                res.append(word)
+            for nei in sorted(node.child):
+                dfs(node.child[nei], word + nei)
         
+        # Enumerating from N - 1
+        
+        for i in range(len(words) , 0, -1):
+            if len(res) >= k:
+                break
+            else:
+                if w_bucket[i]:
+                    node = w_bucket[i]
+                    dfs(node, "")
+        return res[:k]
+            
