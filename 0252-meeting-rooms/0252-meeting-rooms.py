207d1c903d25107fde066a32a194ddc15c9a6df1
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-28 09:55:11 -0500

    Time: 4 ms (33.66%), Space: 19.6 MB (83.87%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,13 +1,19 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals = sorted(intervals)
+        
+        intervals.sort()
         if len(intervals) > 1:
             prev_end = intervals[0][-1]
-            for i in range(1, len(intervals)):
-                
+            for i in range(1, len(intervals)): 
                 if intervals[i][0] < prev_end:
                     return False
                 prev_end = intervals[i][-1]
             return True
         else:
             return True
+        # Time complexity is 
+        # O(n log n) for sorting
+        # and for traversing O (n)
+        # so total will be o(n logn) + o(n)
+        # since o(n logn ) will be dominant my total time complexity would be o(n logn)
+        # Space complexity = O(1) no extra space is being used
