88f637230a2ab852bd98ab28a5e2520aac4bcef6
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-30 14:04:18 -0500

    Time: 28 ms (66.14%), Space: 18.8 MB (38.63%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,6 +1,7 @@
 from collections import Counter
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
+        '''
         n = len(s)
         total_words = len(words)
         word_len = len(words[0])
@@ -24,4 +25,48 @@ class Solution:
                 if words_used == total_words:
                     res.append(i)
         return res
+        '''
+        # Optimal solution - Stream and slide the window if found the excessive words or not valid
+        n = len(s)
+        word_map = Counter(words)
+        win_size = len(words[0])
+        concatenated_len = len(words) * win_size
+        res = []
+
+        def sliding_window(stream):
+            left = right = stream
+            word_count = 0
+            word_map_curr = Counter()
+            
+            # Slide the window
+            while right + win_size <= n:
+                sub = s[right : right + win_size]
+                right += win_size
                 
+                if sub in word_map:
+                    word_map_curr[sub] += 1
+                    
+                    if word_map_curr[sub] <= word_map[sub]:
+                        word_count += 1
+
+                    # Shrink the window if there's an excess word
+                    while word_map_curr[sub] > word_map[sub]:
+                        new_substr = s[left : left + win_size]
+                        word_map_curr[new_substr] -= 1
+                        if word_map_curr[new_substr] < word_map[new_substr]:
+                            word_count -= 1
+                        left += win_size
+                
+                    # If all words are found, add the index to result
+                    if word_count == len(words):
+                        res.append(left)
+                else:
+                    word_count = 0
+                    word_map_curr.clear()
+                    left = right
+
+        # Start sliding windows from different positions
+        for i in range(win_size):
+            sliding_window(i)
+        
+        return res
