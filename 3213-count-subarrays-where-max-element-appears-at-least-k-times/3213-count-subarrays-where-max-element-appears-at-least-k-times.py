e7c88e6273932c817100fa0339383a08e7ebb910
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-30 07:48:25 -0500

    Time: 98 ms (34.06%), Space: 29.7 MB (38.26%) - LeetHub

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/3213-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -3,6 +3,8 @@ class Solution:
         '''
         find the max ele
         '''
+
+        '''
         max_ele = max(nums)
         l, r = 0 , 0
         max_count = 0
@@ -17,3 +19,17 @@ class Solution:
             sub_array_cnt += l
             r += 1
         return sub_array_cnt
+        '''
+        # through index
+        max_ele = max(nums)
+        max_indx = []
+        valid_sub = 0
+        for r in range(len(nums)):
+            if nums[r] == max_ele:
+                max_indx.append(r)
+           
+            if len(max_indx) >= k:
+                valid_sub += max_indx[-k] + 1
+        return valid_sub
+
+
