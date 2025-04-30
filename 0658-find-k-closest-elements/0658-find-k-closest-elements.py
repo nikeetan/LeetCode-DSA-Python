222e7b3f5c78ed641a63a1c5d59606a5e3be72ba
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-30 18:24:23 -0500

    Time: 11 ms (52.19%), Space: 18.9 MB (83.05%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,4 +1,34 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        arr = sorted(arr, key = lambda num : abs( x - num))
-        return sorted(arr[:k])
+        # find the closest using binary search
+        if len(arr) == k:
+            return arr
+
+        l, r = 0, len(arr) - 1
+        ans = -1
+        target= float('inf')
+        while l <= r:
+            mid = l + (r - l)//2
+            if arr[mid] >= x:
+                r = mid - 1
+            else:
+                l = mid + 1
+        
+        print(l, r)
+        # now lets do the sliding window
+        l = l - 1
+        r = l + 1
+
+        while (r - l - 1) < k:
+            if l < 0:
+                r += 1 
+                continue
+            elif r == len(arr) or  abs(arr[l] - x) <= abs(arr[r] - x):
+                l -= 1 
+            else:
+                r += 1 
+        return arr[l+1  : r]
+
+
+
+
