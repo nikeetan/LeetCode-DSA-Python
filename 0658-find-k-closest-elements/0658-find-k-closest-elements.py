27e871b44a3b4bff590d3560963c4332839fcf0b
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-04-30 19:19:05 -0500

    Time: 0 ms (100%), Space: 19.2 MB (47.9%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,30 +1,43 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
         # find the closest using binary search
-        if len(arr) == k:
-            return arr
+        # if len(arr) == k:
+        #     return arr
 
-        l, r = 0, len(arr) - 1
-        while l <= r:
-            mid = l + (r - l)//2
-            if arr[mid] >= x:
-                r = mid - 1
-            else:
-                l = mid + 1
+        # l, r = 0, len(arr) - 1
+        # while l <= r:
+        #     mid = l + (r - l)//2
+        #     if arr[mid] >= x:
+        #         r = mid - 1
+        #     else:
+        #         l = mid + 1
         
-        # now lets do the sliding window
-        l = l - 1
-        r = l + 1
+        # # now lets do the sliding window
+        # print(r, l)
+        # l = l - 1
+        # r = l + 1
+        
+        # while (r - l - 1) < k:
+        #     if l < 0:
+        #         r += 1 
+        #         continue
+        #     elif r == len(arr) or  abs(arr[l] - x) <= abs(arr[r] - x):
+        #         l -= 1 
+        #     else:
+        #         r += 1 
+        # return arr[l+1  : r]
 
-        while (r - l - 1) < k:
-            if l < 0:
-                r += 1 
-                continue
-            elif r == len(arr) or  abs(arr[l] - x) <= abs(arr[r] - x):
-                l -= 1 
+        # first find the starting point it should be in the range of [0, len(arr) - k] to accomodate k elements
+        l, r = 0, len(arr) - k
+
+        while l < r:
+            mid = l + (r - l)//2
+            if (x - arr[mid]) > (arr[mid + k] - x):
+                l = mid + 1
             else:
-                r += 1 
-        return arr[l+1  : r]
+                r = mid 
+        return arr[l : l + k]
+
 
 
 
