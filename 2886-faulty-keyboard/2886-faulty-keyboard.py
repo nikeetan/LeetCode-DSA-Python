7dda9543b3ee5a096b48b999a6b8dd284d3bc6b5
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-02 15:04:37 -0500

    Time: 16 ms (5.81%), Space: 17.8 MB (57.23%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.py
+++ 2886-faulty-keyboard/2886-faulty-keyboard.py
@@ -6,11 +6,29 @@ class Solution:
         s
         t 
         r
+        Time complexity is O(m2) everytime when i hit i i will be reversing and storinh and space complexity is O(n) 
         '''
-        stack = []
-        for curr_char in s:
-            if curr_char != 'i':
-                stack.append(curr_char)
-            else:
-                stack = stack[::-1]
-        return ''.join(stack)
+        # stack = []
+        # for curr_char in s:
+        #     if curr_char != 'i':
+        #         stack.append(curr_char)
+        #     else:
+        #         stack = stack[::-1]
+        # return ''.join(stack)
+
+        # two pointers
+        # convert string to list the movement we encounter the character "i" index will start from left = 0 , right = index - 1
+        s = list(s)
+        l, r = 0, 0
+        index = 0
+        while index < len(s):
+            if s[index] == 'i':
+                r = index - 1
+                l = 0
+                while l < r:
+                    s[l], s[r] = s[r], s[l]
+                    l += 1
+                    r -= 1
+                s[index] = ''
+            index += 1
+        return ''.join(s)
