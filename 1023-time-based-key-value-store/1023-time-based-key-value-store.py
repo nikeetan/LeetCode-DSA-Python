b86c4bd875e9ce71cc478e09f1a9f3584e6226a3
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-03 14:38:43 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.py
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.py
@@ -9,31 +9,32 @@ min_heap
 class TimeMap:
 
     def __init__(self):
-        self.set_word = {}
+        self.store = {} # hash_map to store the TimeMap
      
     def set(self, key: str, value: str, timestamp: int) -> None:
-        # We are setting the first key value pair for the hashmap
-        if key not in self.set_word:
-            self.set_word[key] = []
-        self.set_word[key].append((timestamp,value))
-    def get(self, key: str, timestamp: int) -> str:
-    
-        if key not in self.set_word:
-            return ""
+        if key not in self.store:
+            self.store[key] = [(value, timestamp)]
+        else:
+            self.store[key].append((value, timestamp))
 
-        timeprev = self.set_word[key]
-        #timeprev = [key for key, value in word_dict.items()]
-        
-        low , high = 0, len(timeprev) - 1
-        def search(timeprev, timestamp, low, high):
-            ans = ""
-            while low <= high:
-                mid = low + (high - low)//2
-                if timeprev[mid][0] <= timestamp:
-                    ans = timeprev[mid][1]
-                    low = mid + 1
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.store:
+
+            timestamps = self.store[key] # here the list will be fetched
+            
+            l, r = 0, len(self.store[key]) - 1
+            ans = -1
+            print(timestamps)
+            while l <= r:
+                mid = l + (r - l)//2
+                if timestamps[mid][-1] <= timestamp:
+                    ans = mid
+                    l = mid + 1
                 else:
-                    high = mid - 1
-            return ans
-        ans = search(timeprev, timestamp, low, high)
-        return ans
+                    r = mid - 1
+            if ans == -1:
+                return ""
+            else:
+                return timestamps[ans][0]
+        else:
+            return ""
