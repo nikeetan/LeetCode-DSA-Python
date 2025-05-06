0f95cea511b9a8efed8fefc83ad0486f7f5d948a
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-06 07:51:51 -0500

    Time: 140 ms (51.29%), Space: 33 MB (60.31%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        '''
+        memoization 
+        '''
+        R = len(matrix)
+        C = len(matrix[0])
+        dp = [[0 for i in range(C)] for j in range(R)]
+        max_ans = float('-inf')
+        '''
+        fill out 1st row and col 
+        '''
+        for i in range(C):
+            if matrix[0][i] == '1':
+                dp[0][i] = 1
+            else:
+                dp[0][i] = 0
+            max_ans = max(max_ans, dp[0][i])
+
+        for i in range(R):
+            if matrix[i][0] == '1':
+                dp[i][0] = 1
+            else:
+                dp[i][0] = 0
+            max_ans = max(max_ans, dp[i][0])
+        
+        for i in range(1, R):
+            for j in range(1, C):
+                if matrix[i][j] == '1':
+                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j - 1], dp[i- 1][j]) + 1
+                else:
+                    dp[i][j] = 0
+                max_ans = max(max_ans, dp[i][j])
+        return max_ans * max_ans
