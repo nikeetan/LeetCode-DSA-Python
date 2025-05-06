286e058fe39ede2b9789727d9ab6eda8238bc12c
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-06 10:37:36 -0500

    Time: 35 ms (99.74%), Space: 38.2 MB (34.59%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        '''
+        Brute force is traverse using two loops and find the % if they are equal to j - i + 1 >= 2 then 
+        and take the subarray sum and check divisible by k
+        return True
+        TC : O(n3)
+        SC : O(1)
+        '''
+
+        '''
+        intution when we have 
+        [23,2,4,6,7]
+        23 % 6 = 5
+        25 % 6 = 1 
+        29 % 6 = 5
+        we can see we started with the remainder 5 and we ended up with the same remainder that means to the first number there is some added which is the multiple of k so we will return true
+
+        '''
+        hash_map = {0 : -1}
+        prefix = 0
+        for i in range(len(nums)):
+            prefix = (prefix + nums[i]) % k
+            if prefix in hash_map:
+                if (i - hash_map[prefix]) > 1:
+                    return True
+            else:
+                hash_map[prefix] = i
+
+        return False
