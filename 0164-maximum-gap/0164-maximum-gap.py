7a3166456636aec813f791d5970281860af21b68
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-07 15:40:41 -0500

    Time: 156 ms (73.69%), Space: 28.6 MB (97.43%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -4,12 +4,12 @@ class Solution:
             return 0
         else:
             nums.sort()
-            p1, p2 = 0, 1
+            curr_indx = 1
             maxi = float('-inf')
-            while p2 < len(nums):
-                if nums[p2] - nums[p1] > maxi:
-                    maxi = nums[p2] - nums[p1]
-                p2 += 1
-                p1 += 1
+            while curr_indx < len(nums):
+                if nums[curr_indx] - nums[curr_indx - 1] > maxi:
+                    maxi = nums[curr_indx] - nums[curr_indx - 1]
+                curr_indx += 1
             return maxi
-            
+    # TC : O(n logn) for sorting and then for traversing to find the maximum difference it's o(n)
+    # SC : O(1)
