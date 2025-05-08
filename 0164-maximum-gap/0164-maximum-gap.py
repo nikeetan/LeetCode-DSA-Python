2bb1b39d90e1566d7cfb9119bd93b1fb0cf756ef
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-07 19:31:14 -0500

    Time: 491 ms (24.18%), Space: 30.1 MB (44.17%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,15 +1,31 @@
+import math
 class Solution:
     def maximumGap(self, nums: List[int]) -> int:
         if len(nums) < 2:
             return 0
         else:
-            nums.sort()
-            curr_indx = 1
+            exp = 1
+            while max(nums) // exp > 0:
+                num_map = {i : [] for i in range(10)}
+                for num in nums:
+                    num_map[(num // exp) % 10].append(num)
+                exp = exp * 10
+                curr_indx = 0
+                for key , val in num_map.items():
+                    for i in range(len(val)):
+                        nums[curr_indx] = val[i]
+                        curr_indx += 1 
+
+            '''
+            max_Gap
+            '''
             maxi = float('-inf')
-            while curr_indx < len(nums):
-                if nums[curr_indx] - nums[curr_indx - 1] > maxi:
-                    maxi = nums[curr_indx] - nums[curr_indx - 1]
-                curr_indx += 1
+            for i in range(1, len(nums)):
+                if nums[i] - nums[i - 1] > maxi:
+                    maxi = nums[i] - nums[i -1]
+    
             return maxi
+
+
     # TC : O(n logn) for sorting and then for traversing to find the maximum difference it's o(n)
     # SC : O(1)
