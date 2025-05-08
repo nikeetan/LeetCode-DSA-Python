18303a6f33b041f0af6877c273c6b8e1854acb9e
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-08 09:22:05 -0500

    Time: 140 ms (41.99%), Space: 18.8 MB (12.34%) - LeetHub

---
+++ 3627-find-minimum-time-to-reach-last-room-i/3627-find-minimum-time-to-reach-last-room-i.py
@@ -0,0 +1,32 @@
+import heapq
+class Solution:
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        # classic BFS
+        visited = set()
+        # Row and column 
+        R = len(moveTime)
+        C = len(moveTime[0])
+        # maintianing the queue as  (t, r, c)
+        start = (0, 0 , 0)
+        queue = []
+        heapq.heappush(queue, start)   
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        while queue:
+            curr_t, r, c = heapq.heappop(queue)
+            if (r, c) in visited:
+                continue
+            visited.add((r, c))
+            if ((r == R - 1) and (c == C - 1)):
+                return curr_t
+            for dirX, dirY in directions:
+                nei_x , nei_y = r + dirX, c + dirY
+                if ((0 <= nei_x < R and 0<= nei_y < C) and (nei_x, nei_y) not in visited):
+                    if curr_t + 1 > moveTime[nei_x][nei_y]:
+                        new_t = curr_t + 1 
+                    else:
+                        new_t = moveTime[nei_x][nei_y] + 1
+                    heapq.heappush(queue, (new_t, nei_x, nei_y))
+        return - 1
+        
+
+        
