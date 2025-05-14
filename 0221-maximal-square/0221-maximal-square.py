9caf4df975889570d79c9870c88836439fde7608
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-14 13:09:56 -0500

    Time: 147 ms (46.12%), Space: 32.7 MB (90%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.py
+++ 0221-maximal-square/0221-maximal-square.py
@@ -34,18 +34,42 @@
 #         return max_ans * max_ans
 class Solution:
     def maximalSquare(self, matrix):
-        rows = len(matrix)
-        cols = len(matrix[0])
-        dp = [0] * (cols + 1)
-        maxsqlen = 0
-        prev = 0
-        for i in range(1, rows + 1):
-            for j in range(1, cols + 1):
-                temp = dp[j]
-                if matrix[i - 1][j - 1] == "1":
-                    dp[j] = min(dp[j - 1], prev, dp[j]) + 1
-                    maxsqlen = max(maxsqlen, dp[j])
+        # TC :  O(m x n)
+        # SC :  O(m x n)
+
+        
+        dp = [[0 for i in range(len(matrix[0]))] for i in range(len(matrix))]
+        max_ans = float('-inf')
+        # copy first row corresponding elements 
+
+        # for i in range(len(matrix)):
+        #     if matrix[i][0] == '1':
+        #         dp[i][0] = 1
+        #     else:
+        #         dp[i][0] = 0
+        #     max_ans = max(max_ans, dp[i][0])
+
+        # # copy first col
+        # for i in range(len(matrix[0])):
+        #     if matrix[0][i] == '1':
+        #         dp[0][i] = 1
+        #     else:
+        #         dp[0][i] = 0
+        #     max_ans = max(max_ans, dp[0][i])
+        
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i == 0 or j == 0:
+                    if matrix[i][j] == "1":
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = 0
+                elif matrix[i][j] == "1":
+                    dp[i][j] = min(dp[i-1][j-1],dp[i-1][j], dp[i][j-1]) + 1
                 else:
-                    dp[j] = 0
-                prev = temp
-        return maxsqlen * maxsqlen
+                    dp[i][j] = 0
+                max_ans = max(max_ans, dp[i][j])
+        return max_ans * max_ans
+
+
+        
