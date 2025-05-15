5c77a5ee20093685658d99fb48e73b2181cdb85b
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-14 19:25:06 -0500

    Time: 54 ms (21.1%), Space: 20.1 MB (16.47%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -4,14 +4,26 @@
 #         self.val = x
 #         self.next = None
 
+'''
+if the nodes are unique i can think of keeping a hash_map and then everytime i traverse i will keep check if the curr value is the new value or the old value if there is a point where i am at the node whose value is already seen then i will return true
+TC : O(N) SC : O(1)
+
+fast and slow pointer fast travers two and slow traverse one
+
+[3,2,0,-4]
+ s         
+ f       
+'''
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow=head 
-        fast=head
-        while fast and fast.next:
-            fast=fast.next.next
-            slow=slow.next
-            if fast==slow:
-                return True
-        return False
-    
+        if not head or not head.next:
+            return False
+        else:
+            slow = head
+            fast = head.next 
+            while slow != fast:
+                if ((not fast) or (not fast.next)):
+                    return False
+                slow = slow.next
+                fast = fast.next.next
+            return True 
