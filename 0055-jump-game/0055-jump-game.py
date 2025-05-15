ef3808ede8b60e3086fc48236b9a73a161a5a5f2
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-15 14:32:17 -0500

    Time: 38 ms (38.5%), Space: 18.6 MB (63.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -9,19 +9,28 @@ class Solution:
         #             return True
         #     return False
         # return helper(0)
+        # n = len(nums)
+        # queue = deque([0])
+        # visited = set([0])
+        # while queue:
+        #     curr_indx = queue.popleft()
+        #     if curr_indx + 1 >= n:
+        #         return True
+        #     for nei_indx in range(1, nums[curr_indx] + 1):
+        #         new_indx = curr_indx + nei_indx
+        #         if new_indx >= n:
+        #             return True
+        #         if new_indx not in visited:
+        #             queue.append(new_indx)
+        #             visited.add(new_indx)
+        # return False
+        # at any point we will take the maximum
         n = len(nums)
-        queue = deque([0])
-        visited = set([0])
-        while queue:
-            curr_indx = queue.popleft()
-            if curr_indx + 1 >= n:
-                return True
-            for nei_indx in range(1, nums[curr_indx] + 1):
-                new_indx = curr_indx + nei_indx
-                if new_indx >= n:
-                    return True
-                if new_indx not in visited:
-                    queue.append(new_indx)
-                    visited.add(new_indx)
-        return False
+        far = 0
+        for i in range(len(nums)):
+            if far < i:
+                return False
+            far = max(far , i + nums[i])
+        return True
+
 
