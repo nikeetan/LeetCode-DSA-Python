caf0ca54d5b9df473bdfeb566d1a1b910ab53076
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-15 14:07:03 -0500

    Time: 2261 ms (11.43%), Space: 19.6 MB (6.46%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,11 +1,27 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        @cache
-        def helper(indx):
-            if indx + 1 >= len(nums):
+        # @cache
+        # def helper(indx):
+        #     if indx + 1 >= len(nums):
+        #         return True
+        #     for i in range(1, nums[indx] + 1):
+        #         if helper(indx + i):
+        #             return True
+        #     return False
+        # return helper(0)
+        n = len(nums)
+        queue = deque([0])
+        visited = set([0])
+        while queue:
+            curr_indx = queue.popleft()
+            if curr_indx + 1 >= n:
                 return True
-            for i in range(1, nums[indx] + 1):
-                if helper(indx + i):
+            for nei_indx in range(1, nums[curr_indx] + 1):
+                new_indx = curr_indx + nei_indx
+                if new_indx >= n:
                     return True
-            return False
-        return helper(0)
+                if new_indx not in visited:
+                    queue.append(new_indx)
+                    visited.add(new_indx)
+        return False
+
