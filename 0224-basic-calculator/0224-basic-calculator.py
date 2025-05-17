c90d75525baa4ec59b108f7b317443d1e050ce13
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-17 10:20:03 -0500

    Time: 23 ms (74.25%), Space: 18.4 MB (63.47%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.py
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -1,42 +1,27 @@
 class Solution: 
-
-    def calculator(self, stack) -> int:
-        if not stack or type(stack[-1]) == str:
-            stack.append(0)
-        res = stack.pop()
-        while stack and stack[-1] != ')':
-            sign = stack.pop()
-            if sign == '+':
-                res += stack.pop()
-            else:
-                res -= stack.pop()
-
-        return res
         
     def calculate(self, s: str) -> int:
-        stack = []
-        n, operand = 0, 0
-        char = ""
-        for i in range(len(s)- 1, -1 , -1):
-            char = s[i]
-            print(char)
-
-            if char.isdigit():
-                operand = (10**n * int(char)) + operand
-                n += 1
+        stk = []
+        curr_num, sign, res = 0 , 1, 0
+        for ch in s:
+            if ch.isdigit():
+                curr_num = curr_num * 10 + int(ch)
             
-            elif char != ' ':
-                if n:
-                    stack.append(operand)
-                    n, operand = 0, 0
+            if ch in '+-':
+                res = curr_num * sign + res
+                sign = -1 if ch == '-' else 1
+                curr_num = 0
+            elif ch =='(':
+                stk.append(res)
+                stk.append(sign)
+                sign = 1
+                res = 0
+            elif ch ==')':
+                res = res + curr_num * sign
+                res *= stk.pop()
+                res += stk.pop()
+                curr_num = 0
+        return res + curr_num * sign
+            
 
-                if char =='(':
-                    res = self.calculator(stack)
-                    stack.pop()
-                    stack.append(res)
-                else:
-                    stack.append(char)
-        if n:
-            stack.append(operand)
-        return self.calculator(stack)
 
