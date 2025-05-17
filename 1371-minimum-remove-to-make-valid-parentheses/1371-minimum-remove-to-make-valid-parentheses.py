2dea96736fe918913c711ade5dd794f4c9e95f6c
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-16 21:22:22 -0500

    Time: 39 ms (71.42%), Space: 19.9 MB (24.21%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -1,37 +1,26 @@
+#TC : O(n)
+#SC : O(n)
+
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        # Garreeb approach
-        p1=0
-        stack=[]
-        while p1<len(s):
-            if s[p1]==')':
-                if stack and stack[-1][0]=='(':
-                    stack.pop()
+        s = list(s)
+        open_b = 0
+        indx_to_remove = []
+        for i, val in enumerate(s):
+            if val == '(':
+                open_b += 1
+            elif val == ')':
+                if open_b == 0:
+                    s[i] = '*'
                 else:
-                    stack.append([s[p1],p1])
-            if s[p1]=='}':
-                if stack and stack[-1][0]=='}':
-                    stack.pop()
-                else:
-                    stack.append([s[p1],p1])
-            if s[p1]==']':
-                if stack and stack[-1][0]==']':
-                    stack.pop()
-                else:
-                    stack.append([s[p1],p1])    
-            if s[p1]=='(' or s[p1]=='{' or s[p1]=='[':
-                stack.append([s[p1],p1])
-            p1+=1
-        indices=[]
-        for i in stack:
-            indices.append(i[-1])
-        new_string=""
-        for i in range(len(s)):
-            if i not in indices:
-                if new_string:
-                    new_string+=s[i]
-                else:
-                    new_string=s[i]
-            else:
-                continue
-        return new_string
+                    open_b -= 1
+        if open_b > 0:
+            for i in range(len(s)- 1, -1, -1):
+                if open_b == 0:
+                    break
+                if s[i] == '(':
+                    s[i] = '*'
+                    open_b -= 1
+        
+        return "".join(c for c in s if c != '*')
+                
