9b404f2958e36ec9277bf3571c9a33274d71d451
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-17 13:36:47 -0500

    Time: 90 ms (30.13%), Space: 21.8 MB (45.09%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,12 +1,50 @@
 '''
+3 + 2 * 2
+      p
 
-[2, -22, 33, 44]
-44 + 33 + (-22) + 2
+cur_num = 3 
+sign = 1 
+res = 0
+
+cur_num = 3 
+sign = 1 
+res = 3
+
+cur_num = 2
+sign = 1 
+res = 3
+
+
 
 '''
 
 class Solution:
     def calculate(self, s: str) -> int:
+        curr_num = 0
+        sign = '+'
+        stk = []
+        for ch in range(len(s)):
+            if s[ch].isdigit():
+                curr_num = curr_num * 10 + int(s[ch])
+            if s[ch] in '+-*/' or ch == len(s) - 1:
+                if sign in '+-':
+                    stk.append(-curr_num) if sign == '-' else stk.append(curr_num)
+                elif sign == '*':
+                    stk.append(stk.pop() * curr_num)
+                elif sign =='/':
+                    stk.append(int(stk.pop() / curr_num))
+                sign = s[ch]
+                curr_num = 0
+        print(stk)
+        return sum(stk)
+
+
+            
+
+        
+
+
+        '''
         # Initialize stack
         res = 0
         last_num = 0
@@ -41,5 +79,37 @@ class Solution:
                 curr_num = 0           # 0 
             indx += 1
         return res + last_num
+        '''
 
 
+'''
+3-2*2
+res     = 0         
+las_num = 0 
+curr_num = 3
+curr_char = 3
+oper = +
+
+res     = 0 
+las_num = 3
+curr_num = 0
+oper = -
+
+res     = 0
+las_num = 3
+curr_num = 2
+oper = -
+
+res     = 3
+las_num = -2
+curr_num = 0
+oper = *
+
+res     = 3
+las_num = - 4
+curr_num = 0
+oper = *
+
+
+res + last_num 
+'''
