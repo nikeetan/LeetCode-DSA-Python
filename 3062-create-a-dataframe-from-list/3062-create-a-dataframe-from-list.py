5064769d952b8986ed42416e86fc94be6775492a
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-16 19:10:06 -0500

    Time: 269 ms (43.74%), Space: 65.6 MB (80.52%) - LeetHub

--- 3062-create-a-dataframe-from-list/3062-create-a-dataframe-from-list.py
+++ 3062-create-a-dataframe-from-list/3062-create-a-dataframe-from-list.py
@@ -8,9 +8,9 @@ Student_df = { "Student_id" : [ 1, 2, 3 ], "age" : [ 10, 20 , 30]}
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     student_df = { "student_id" : [], "age" : []}
     for data in student_data:
-        curr_std_id, curr_std_age = data
-        student_df["student_id"].append(curr_std_id)
-        student_df["age"].append(curr_std_age)
+       
+        student_df["student_id"].append(data[0])
+        student_df["age"].append(data[1])
     student_df = pd.DataFrame(student_df)
     return student_df
     
