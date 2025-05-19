8042054d4de77991d75b95fc28a8982d1d3f2235
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-18 21:33:47 -0500

    Time: 631 ms (95.66%), Space: 55.8 MB (99.34%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.py
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.py
@@ -7,14 +7,14 @@ class Solution:
         # start point will be where i have water cell        
         R = len(isWater)
         C = len(isWater[0])
-        res = [[ -1 for i in range(C)] for j in range(R)]
         queue = deque()
         for r in range(R):
             for c in range(C):
                 if isWater[r][c] == 1:
-                    res[r][c] = 0
                     queue.append((r, c))
-                    #isWater[r][c] = 0
+                    isWater[r][c] = 0
+                else:
+                    isWater[r][c] = -1
         curr_height = 1
         while queue:
             layer = len(queue)
@@ -25,10 +25,10 @@ class Solution:
                     nei_x = curr_x + dirX
                     nei_y = curr_y + dirY
                     # checking inbounds
-                    if ((0 <= nei_x < R) and (0 <= nei_y < C)) and res[nei_x][nei_y] == -1:
-                        res[nei_x][nei_y] = curr_height
+                    if ((0 <= nei_x < R) and (0 <= nei_y < C)) and isWater[nei_x][nei_y] == -1 :
+                        isWater[nei_x][nei_y] = curr_height
                         queue.append((nei_x, nei_y))
             curr_height += 1
-        return res
+        return isWater
 
 
