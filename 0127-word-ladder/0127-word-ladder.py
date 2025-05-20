6892f22866c2a5fb95f02beb3b4911a5b35dfe8b
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-20 10:00:39 -0500

    Time: 25 ms (99.59%), Space: 18.4 MB (98.96%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -3,26 +3,36 @@ class Solution:
         '''
         everytime there is a char changed by position i will add it in a queue if its not visited we can perform BFS
         '''
-        queue = deque([(beginWord, 1)])
-        chars = "abcdefghijklmnopqrstuvwxyz"
-        visited = set()
-        wordList = set(wordList)
-        while queue:
-            word, level = queue.popleft()
-            visited.add(word)
-            # now word transformation lets say i have hit
-            # i will covert into list of character so ['h' ,'i' ,'t']
-            # ['a','i','t']['b','i','t'],['c','i','t']
-            if word == endWord:
-                return level 
-            pos = 0
-            while pos < len(word):
-                temp = list(word)
-                for i in chars:
-                    temp[pos] = i
-                    new_word = ''.join(temp)
-                    if (new_word not in visited) and (new_word in wordList):
-                        queue.append((new_word, level+1))
-                pos += 1
-        return 0
-                
+        if endWord not in wordList:
+            return 0
+        else:
+            front = {beginWord}
+            back = {endWord}
+            chars = "abcdefghijklmnopqrstuvwxyz"
+            visited = set()
+            wordList = set(wordList)
+            steps = 1
+            while front and back:
+                if len(front) > len(back):
+                    front, back = back, front
+                next_front = set()
+                for word in front:
+                # now word transformation lets say i have hit
+                # i will covert into list of character so ['h' ,'i' ,'t']
+                # ['a','i','t']['b','i','t'],['c','i','t']
+                    pos = 0
+                    while pos < len(word):
+                        temp = list(word)
+                        for i in chars:
+                            temp[pos] = i
+                            new_word = ''.join(temp)
+                            if new_word in back:
+                                return steps + 1
+                            if (new_word not in visited) and (new_word in wordList):
+                                next_front.add(new_word)
+                                visited.add(new_word)
+                        pos += 1
+                front = next_front
+                steps += 1
+            return 0
+                    
