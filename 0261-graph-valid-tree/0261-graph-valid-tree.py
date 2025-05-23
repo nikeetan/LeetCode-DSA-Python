4ad2ed60b8e23e4de3e5fbda2af952525a9943c9
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-22 19:51:54 -0500

    Time: 3 ms (48.55%), Space: 18.7 MB (96.49%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -9,16 +9,31 @@ class Solution:
             adj[u].append(v)
             adj[v].append(u)
         
-        visited = [False] * n
-        stk = []
-        visited[0] = True
-        stk.append(0)
+        visited = set()
+        def dfs(node):
+            stk = []
+            stk.append(node)
+            visited.add(node)
+            while stk:
+                curr_node = stk.pop()
+                for nei in adj.get(curr_node, []):
+                    if nei not in visited:
+                        visited.add(nei)
+                        stk.append(nei)
+        dfs(0)
+        return len(visited) == n
+
         
-        while stk:
-            node = stk.pop()
-            for nei in adj.get(node, []):
-                if not (visited[nei]):
-                    visited[nei] = True
-                    stk.append(nei)
-        return all(visited)
+        # visited = [False] * n
+        # stk = []
+        # visited[0] = True
+        # stk.append(0)
+        
+        # while stk:
+        #     node = stk.pop()
+        #     for nei in adj.get(node, []):
+        #         if not (visited[nei]):
+        #             visited[nei] = True
+        #             stk.append(nei)
+        # return all(visited)
         
