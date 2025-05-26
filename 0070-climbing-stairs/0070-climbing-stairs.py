1442718fd1c24ca078d33392c7e4dc62480f0979
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-26 17:47:07 -0500

    Time: 0 ms (100%), Space: 17.9 MB (8%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,14 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 1:
-            return 1
-        dp = [0 for i in range(n+1)]
-        dp[0] = 1
-        dp[1] = 1
-        steps = [1, 2]
-        for i in range(2, n+1):
-            for step in steps:
-                if i - step >= 0:
-                    dp[i] += dp[i-step]
-        return dp[n]
-       
+        memo = {}
+        def helper(indx):
+            if indx in memo:
+                return memo[indx]
+            if indx == n:
+                return 1
+            if indx > n:
+                return 0
+            memo[indx] = helper(indx + 1) + helper(indx + 2)
+            return memo[indx]
+        return helper(0)
