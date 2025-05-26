5365ae7add44d7e33224082fe14cbf9170a581f9
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-26 17:07:29 -0500

    Time: 0 ms (100%), Space: 18.1 MB (36.74%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,18 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
-        nums.sort()  # Sort to group duplicates
+        path = []
+        nums.sort()  
         
-        def backtrack(indx, path):
+        def backtrack(indx):
             res.append(path[:])  # Append a copy of path
             
             for i in range(indx, len(nums)):
-                # Skip duplicates at the same tree level
+               
                 if i > indx and nums[i] == nums[i - 1]:
                     continue
                 path.append(nums[i])
-                backtrack(i + 1, path)
+                backtrack(i + 1)
                 path.pop()  # Backtrack
 
-        backtrack(0, [])
+        backtrack(0)
         return res
