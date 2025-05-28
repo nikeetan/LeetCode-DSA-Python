967e74aa39322c42f900ef06bda17d0ed71e3cee
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-28 14:23:00 -0500

    Time: 10 ms (6.76%), Space: 18.1 MB (40.22%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,29 +1,41 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         '''
-        so if it was sort
+        symmetry
         '''
-        res = []
-        p1, p2 = 0, 0
-        def merge(nums1, nums2, p1, p2):
-            while p1 < len(nums1) and p2 < len(nums2):
-                if nums1[p1] < nums2[p2]:
-                    res.append(nums1[p1])
-                    p1 += 1
-                else:
-                    res.append(nums2[p2])
-                    p2 += 1
-          
-            while p1 < len(nums1):
-                res.append(nums1[p1])
-                p1 += 1
+        if len(nums2) < len(nums1):
+            nums1, nums2 = nums2, nums1
+        n1 = len(nums1)
+        n2 = len(nums2)
+        left = (n1 + n2 + 1) // 2
+
+        # How many elements possible from first array
+        l, r = 0, n1
+        n = n1 + n2
+        while l <= r:
+            mid1 = (l + r) // 2
+            mid2 = left - mid1
+
+            l1, l2, r1, r2 = float('-inf'), float('-inf'), float('inf'), float('inf')
+            print("info", left, (mid1, mid2))
+            if mid1 < n1:
+                r1 = nums1[mid1]
+            if mid2 < n2:
+                r2 = nums2[mid2]
+            if mid1 - 1 >= 0:
+                l1 = nums1[mid1 - 1]
+            if mid2 - 1 >= 0:
+                #print(nums2, mid2)
+                l2 = nums2[mid2 - 1]
             
-            while p2 < len(nums2):
-                res.append(nums2[p2])
-                p2 += 1
-        merge(nums1, nums2, p1, p2)
-        if len(res)% 2 != 0:
-            return res[len(res)//2]
-       #print(res[len(res)//2] , res[(len(res)//2) - 1])
-        return (res[(len(res)//2) - 1] + res[len(res)//2])/2
-        
+            if l1 <= r2 and l2 <= r1:
+                if n % 2 == 0:
+                    print(l1, l2, r1, r2)
+                    return (max(l1, l2) + min(r1, r2)) / 2
+                else:
+                    return max(l1, l2)
+            elif l1 > r2:
+                r = mid1 - 1
+            else:
+                l = mid1 + 1
+        return 0
