ea0c93debd14c290b010c26e26bc1a7707cffe9c
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-28 17:13:54 -0500

    Time: 3 ms (79.4%), Space: 21.4 MB (29.76%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -2,6 +2,37 @@ import heapq
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
         n = len(matrix)
+        def elemnt_cnt(mid, small, large):
+            row = n - 1
+            col = 0
+            cnt = 0 
+            while row >= 0 and col < n:
+                if matrix[row][col] > mid:
+                    large = min(large, matrix[row][col])
+                    row -= 1
+                else:
+                    small = max(matrix[row][col], small)
+                    cnt += row + 1 # full row elements
+                    col += 1
+            return cnt, small, large
+
+       
+        low = matrix[0][0]
+        high = matrix[n - 1][n - 1]
+        while low < high:
+            mid = low + (high - low) // 2
+            small, large = matrix[0][0], matrix[n - 1][n - 1]
+            cnt, small, large = elemnt_cnt(mid, small, large)
+            if cnt == k:
+                return small
+            elif cnt > k:
+                high = small
+            else:
+                low = large
+        return low
+
+        '''
+        n = len(matrix)
         rows = min(n, k)
         queue = []
         for row in range(rows):
@@ -13,3 +44,5 @@ class Solution:
                 heapq.heappush(queue, (matrix[row][col + 1], row, col + 1))
             k -= 1
         return element
+        '''
+
