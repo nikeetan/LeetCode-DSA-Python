5996e15df7bfa7e71d9b9ac7d0ae19445d91043c
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-28 14:23:22 -0500

    Time: 4 ms (35.45%), Space: 18.1 MB (59.4%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -15,9 +15,7 @@ class Solution:
         while l <= r:
             mid1 = (l + r) // 2
             mid2 = left - mid1
-
             l1, l2, r1, r2 = float('-inf'), float('-inf'), float('inf'), float('inf')
-            print("info", left, (mid1, mid2))
             if mid1 < n1:
                 r1 = nums1[mid1]
             if mid2 < n2:
@@ -25,12 +23,10 @@ class Solution:
             if mid1 - 1 >= 0:
                 l1 = nums1[mid1 - 1]
             if mid2 - 1 >= 0:
-                #print(nums2, mid2)
                 l2 = nums2[mid2 - 1]
             
             if l1 <= r2 and l2 <= r1:
                 if n % 2 == 0:
-                    print(l1, l2, r1, r2)
                     return (max(l1, l2) + min(r1, r2)) / 2
                 else:
                     return max(l1, l2)
