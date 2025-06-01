9d0dbbd67a691b399b815ae064aac08418f9db8b
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-01 16:37:53 -0500

    Time: 0 ms (100%), Space: 17.7 MB (66.9%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,5 +1,22 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        dp = defaultdict(list)
+        for i in range(len(s) - 1, -1 , -1):
+            valid_sentence = []
+            for w in wordDict:
+                if i + len(w) <= len(s) and s[i : i + len(w)] == w:
+                    if i + len(w) == len(s):
+                        valid_sentence.append(w)
+                    else:
+                        sentences = dp[i + len(w)]
+                        for sentence in sentences:
+                            valid_sentence.append(w + " " + sentence)
+
+            dp[i] = valid_sentence
+        return dp[0]
+        
+
+        '''
         res = []
         part = []
         wordDict = set(wordDict)
@@ -15,3 +32,4 @@ class Solution:
                     part.pop()
         dfs(0, [])
         return res            
+        '''
