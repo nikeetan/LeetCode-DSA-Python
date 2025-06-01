5c6c27bbcf67b324a764ae18ef714f32bd4e3154
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-05-31 21:08:50 -0500

    Time: 3 ms (20.92%), Space: 17.6 MB (89.4%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.py
@@ -5,38 +5,21 @@ hills set
 
 class Solution:
     def countHillValley(self, nums: List[int]) -> int:
+        filtered = [nums[0]]
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                continue
+            filtered.append(nums[i])
+        
+        
         hillset = set()
         valleyset = set()
 
-        for i in range(1, len(nums) - 1):
-            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
+        for i in range(1, len(filtered) - 1):
+            if filtered[i] > filtered[i - 1] and filtered[i] > filtered[i + 1]:
                 hillset.add((i - 1, i + 1))
-            elif nums[i] < nums[i - 1] and nums[i] < nums[i + 1]:
+            elif filtered[i] < filtered[i - 1] and filtered[i] < filtered[i + 1]:
                 valleyset.add((i - 1, i + 1))
-            
-            elif nums[i] == nums[i - 1] or nums[i] == nums[i + 1]:
-                j = i
-                if nums[i] == nums[i - 1]:
-                    curr = nums[i]
-                    while j > 0 and curr == nums[j]:
-                        j -= 1
-                    if j < 0:
-                        continue
-                    if nums[i] > nums[j] and nums[i] > nums[i + 1]:
-                        hillset.add((j, i + 1))
-                    elif nums[i] < nums[j] and nums[i] < nums[i + 1]:
-                        valleyset.add((j, i + 1))
-                else:
-                    curr = nums[i]
-                    while j < len(nums) and curr == nums[j]:
-                        j += 1
-                    if j == len(nums):
-                        continue
-                    if nums[i] > nums[i - 1] and nums[i] > nums[j]:
-                        hillset.add((i - 1, j))
-                    elif nums[i] < nums[i - 1] and nums[i] < nums[j]:
-                        valleyset.add((i - 1, j))
         return len(hillset) + len(valleyset)
-                        
-            
-        
+        #'''
+# TC : O(N )
