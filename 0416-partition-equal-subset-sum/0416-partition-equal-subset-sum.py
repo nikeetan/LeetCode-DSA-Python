6e3b6603cfc27d52cd8fb7947aa2dc7b79c6eef9
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-02 10:22:24 -0500

    Time: 1148 ms (26.41%), Space: 33.3 MB (36.67%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        '''
         memo = {}
         def dfs(indx, subset_sum):
             if (subset_sum, indx) in memo:
@@ -17,3 +18,24 @@ class Solution:
             return False
         subset_sum = total_sum // 2
         return dfs(0, subset_sum)
+        '''
+        total = sum(nums)
+
+        if total % 2 != 0:
+            return False
+        else:
+            subset_sum = total // 2
+            dp = [[False] * (subset_sum + 1) for _ in range(len(nums) + 1)]
+            dp[0][0] = True
+            for i in range(1, len(nums) + 1):
+                dp[i][0] = True
+                if i <= subset_sum:
+                    dp[0][i] = False
+            for i in range(1, len(nums) + 1):
+                curr = nums[i - 1]
+                for j in range(1, subset_sum + 1):
+                    if j < curr:
+                        dp[i][j] = dp[i - 1][j]
+                    else:
+                        dp[i][j] = dp[i - 1][j] or dp[i - 1][j - curr]
+            return dp[len(nums)][subset_sum]
