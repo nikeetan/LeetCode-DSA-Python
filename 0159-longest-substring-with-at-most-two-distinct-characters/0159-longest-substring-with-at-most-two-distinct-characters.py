28b9aed7776b02f62c84160929411fd8300edb22
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-03 08:59:08 -0500

    Time: 211 ms (56.82%), Space: 18.1 MB (43.81%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
@@ -10,12 +10,27 @@ then i will move the first pointer
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
         ans = float('-inf')
-        for i in range(len(s)):
-            d = {}
-            for j in range(i, len(s)):
-                d[s[j]] = 1
-                if len(d) > 2:
-                    break
-                ans = max(ans, j - i + 1)
-        return ans
+        left, right = 0, 0
+        d = collections.defaultdict((int))
+        while right < len(s):
+            d[s[right]] += 1
+            while len(d) > 2 and left < right:
+                if d[s[left]] - 1 == 0:
+                    del d[s[left]]
+                else:
+                    d[s[left]] -= 1
+                left += 1
+            ans = max(ans, right - left + 1)
+            right += 1
+        return ans 
+
+        
+        # for i in range(len(s)):
+        #     d = {}
+        #     for j in range(i, len(s)):
+        #         d[s[j]] = 1
+        #         if len(d) > 2:
+        #             break
+        #         ans = max(ans, j - i + 1)
+        # return ans
         
