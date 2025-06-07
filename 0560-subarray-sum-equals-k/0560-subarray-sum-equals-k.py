95ffc1efa5254cd0d96d33cbd51b8099e7af6aef
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-07 12:26:49 -0500

    Time: 32 ms (55.68%), Space: 20.3 MB (93.23%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,14 +1,26 @@
+'''
+1 intra - 2 tagdre 3 barteti ?
+
+[1, -1, 0] K = 0
+running_sum = 1
+is 1 == k no is 1 - 3 = -2 in k
+num_map = { 1  :  1, 2 : 1, 3 : 1}
+
+'''
+
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        ps=0
-        ps_map={0:1}
-        count=0
+        num_map = {0 : 1}
+        cnt = 0
+        running_sum = 0
         for i in nums:
-            ps+=i
-            if ps-k in ps_map:
-                count+=ps_map[ps-k]
-            if ps not in ps_map:
-                ps_map[ps]=1
+            running_sum += i
+            if running_sum - k in num_map:
+                cnt += num_map[running_sum - k]
+            if running_sum not in num_map:
+                num_map[running_sum] = 1
             else:
-                ps_map[ps]+=1
-        return count
+                num_map[running_sum] += 1
+        return cnt
+
+            
