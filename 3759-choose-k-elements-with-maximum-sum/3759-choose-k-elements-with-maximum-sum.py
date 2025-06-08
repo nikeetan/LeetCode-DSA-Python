23389daba5c21b19c502a297ce0af0a67e5660f7
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-08 18:18:42 -0500

    Time: 438 ms (75.4%), Space: 50.6 MB (60.96%) - LeetHub

--- 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.py
+++ 3759-choose-k-elements-with-maximum-sum/3759-choose-k-elements-with-maximum-sum.py
@@ -1,24 +1,26 @@
 import heapq
 class Solution:    
     def findMaxSum(self, nums1: List[int], nums2: List[int], k: int) -> List[int]:
-        num_to_i = {}
-        res  = [0] * len(nums1)
-        for indx, val in enumerate(nums1):
-            if val not in num_to_i:
-                num_to_i[val] = [indx]
-            else:
-                num_to_i[val].append(indx)
-        
+        new_array = []
+        n = len(nums1)
+        res = [0] * n
+        for i in range(n):
+            new_array.append((nums1[i], nums2[i], i))
+        new_array.sort()
         max_heap = []
         running_sum = 0
-        for  val1, val2 in sorted(zip(nums1, nums2)):
-            for i in num_to_i[val1]:
-                res[i] = running_sum
-            num_to_i[val1] = []
-            running_sum += val2
-            heapq.heappush(max_heap, (val2, val1))
+        for i  in range(len(new_array)):
+            nums1_val, nums2_val, res_indx = new_array[i]
+            if i > 0 and (new_array[i - 1][0] == new_array[i][0]):
+                res[res_indx] = res[new_array[i - 1][-1]]
+            else:
+                res[res_indx] = running_sum
+            running_sum += nums2_val
+            heapq.heappush(max_heap, nums2_val)
             if len(max_heap) > k:
-                value, _ = heapq.heappop(max_heap)
-                running_sum -= value
+                # to remove the excess value
+                excess_val = heapq.heappop(max_heap)
+                running_sum -= excess_val
         return res
+        
 
