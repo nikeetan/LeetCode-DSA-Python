3f72b5ef3bb649a59618c0cc9855c7fc8eb75416
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-10 16:27:56 -0500

    Time: 39 ms (43.78%), Space: 19.3 MB (39.46%) - LeetHub

--- 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/2191-minimum-number-of-food-buckets-to-feed-the-hamsters.py
+++ 2191-minimum-number-of-food-buckets-to-feed-the-hamsters/2191-minimum-number-of-food-buckets-to-feed-the-hamsters.py
@@ -1,38 +1,46 @@
 class Solution:
     def minimumBuckets(self, hamsters: str) -> int:
-        
+        hamsters = list(hamsters)
+        n = len(hamsters)
+        for i in range(n):
+            if hamsters[i] == 'H':
+                if i > 0 and hamsters[i - 1] == 'B':
+                    continue
+                elif i < n - 1 and hamsters[i + 1] == '.':
+                    hamsters[i + 1] = 'B'
+                elif i > 0 and hamsters[i - 1] == '.':
+                    hamsters[i - 1] = 'B'
+                else:
+                    return -1
+        food_count = 0
+        for i in hamsters:
+            if i == 'B':
+                food_count += 1
+        return food_count
+        '''
         food_bucket = 0
         seen = set()
         n = len(hamsters)
         for i in range(n):
-            # print(seen)
-            # one index that is ['.']
             if i - 1 < 0:
                 if hamsters[i] == 'H' and i + 1 >= n:
                     return -1
                 elif hamsters[i] == '.'  and i + 1 >= n:
-                    return food_bucket
-            # last index ['H', '.', '.', 'H', '.', '.', ,'H']
-            
+                    return food_bucket            
             if i + 1 >=n: 
-                
                 if hamsters[i] == 'H' and hamsters[i - 1] == 'H':
                     return - 1
                 elif hamsters[i] == 'H' and (i - 1) not in seen:
                     seen.add(i - 1)
                     food_bucket += 1
-                    
                 return food_bucket 
-                
-
-            # Not possible (if i am at hamsters and adjacent i don't have empty space)
+    
             if hamsters[i] == 'H':
                 if (i - 1 > 0) and (hamsters[i - 1] != '.'  and hamsters[i + 1] != '.'):
                         return -1
                 elif (i - 1 < 0) and hamsters[i + 1] != '.':
                         return -1
-                else:
-                   
+                else:               
                     if hamsters[i + 1] == '.':
                         if i - 1 not in seen:
                             seen.add(i + 1)
@@ -41,6 +49,6 @@ class Solution:
                         if i - 1 not in seen:
                             seen.add(i - 1)
                             food_bucket += 1
-    
         return food_bucket
+        '''
             
