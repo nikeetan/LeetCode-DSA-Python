99037ec55a04d1b6e0e247d0d64ff89f7af4b323
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-11 15:25:35 -0500

    Time: 2 ms (89.72%), Space: 18 MB (18.28%) - LeetHub

--- 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.py
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.py
@@ -1,10 +1,18 @@
+'''
+can i improvise this o(n2) ~ o(n)
+'''
+
 from collections import defaultdict
 class Solution:
     def countKDifference(self, nums: List[int], k: int) -> int:
+        num_map = defaultdict(int)
         valid_pairs = 0
-        for i in range(len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if abs(nums[i] - nums[j]) == k:
-                    valid_pairs += 1
+        for num in nums:
+            
+            if abs(num - k) in num_map:
+                valid_pairs += num_map[num - k]
+            if (num + k) in num_map:
+                valid_pairs += num_map[num + k]
+            num_map[num] += 1
         return valid_pairs
-                    
+                
