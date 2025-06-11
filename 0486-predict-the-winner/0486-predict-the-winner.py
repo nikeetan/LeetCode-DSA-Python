adaa2a7a1b04a193b64497906549d8d46b403b21
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-11 13:40:33 -0500

    Time: 0 ms (100%), Space: 17.9 MB (46.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -6,13 +6,14 @@ what am i doing wrong in here adding index no i should be adding + 1 or - 1 for 
 '''
 
 class Solution:
-    def predictTheWinner(self, nums: List[int]) -> bool:
-        n = len(nums) - 1
+    
+    def predictTheWinner(self, nums: List[int]) -> bool:   
+        @cache
         def helper(l, r):
             if l == r:
                 return nums[l]
             return max(nums[l] - helper(l + 1, r), nums[r] - helper(l, r - 1))
-        return helper(0, n) >= 0
+        return helper(0, len(nums) - 1) >= 0
 
 
             
