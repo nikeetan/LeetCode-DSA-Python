83542584abdca61cc238778d5b83ef9bd1137a39
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-14 12:20:54 -0500

    Time: 3 ms (54.29%), Space: 18.2 MB (16.58%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -3,16 +3,21 @@
 tens place we can see 9 so basically we can group the number according to the first digit and sort among themselves which is bucket sort
 '''
 
+import heapq
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-
-        def compare(n1, n2):
-            if n1 + n2 > n2 + n1:
-                return -1
-            else:
-                return 1
-        for i, val in enumerate(nums):
+        res = ""
+        class comparator(str):
+            def __lt__(self, other):
+                # a + b > b + a
+                return self + other > other + self
+        for  i, val in enumerate(nums):
             nums[i] = str(val)
+        max_heap = []
+        for i in nums:
+            heapq.heappush(max_heap, comparator(i))
+        while max_heap:
+            res += heapq.heappop(max_heap)
+        return "0" if len(res) == 0 or res[0] == '0' else res
         
-        nums = sorted(nums, key=cmp_to_key(compare))
-        return str(int("".join(nums)))
+            
