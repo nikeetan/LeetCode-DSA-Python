1f89bbc99a7f8d879b18f75ee94bafe1a6177294
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-15 15:22:52 -0500

    Time: 0 ms (100%), Space: 18 MB (77.34%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.py
@@ -8,6 +8,7 @@ class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         '''
         BFS if the level is divisible by 2 reverse else the same
+        i Know for every level 2 * i + 1 gives me the left child
         '''
         if root is None:
             return []
@@ -21,15 +22,15 @@ class Solution:
             level_nodes = []
             for i in range(lvl):
                 node = queue.popleft()
-                level_nodes.append(node.val)
+                if level_cnt % 2 != 0:
+                    level_nodes.insert(0, node.val)
+                else:
+                    level_nodes.append(node.val)
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            if level_cnt % 2 == 0:
-                res.append(level_nodes)
-            else:
-                res.append(level_nodes[::-1])
+            res.append(level_nodes)
             level_cnt += 1
         return res
 
