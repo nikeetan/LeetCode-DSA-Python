6bfd91d7c5cd7d40a48da82bec7d1f0ea64f826d
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-16 08:05:49 -0500

    Time: 0 ms (100%), Space: 19.9 MB (30.41%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,29 +6,10 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # def validate(root, low , high):
-        #     if root is None:
-        #         return True
-        #     else:
-        #         if (low != None and root.val <= low ) or (high != None and root.val >= high):
-        #             return False
-        #         return validate(root.left, low, root.val)  and validate(root.right, root.val, high)
-        # return validate(root, None, None)
-        prev = float('-inf')
-        def inorder(root):
-            if root is None:
-                return []
-            left = inorder(root.left)
-            node = [root.val]
-            right = inorder(root.right)
-            return left + node + right
-        values = inorder(root)
-        new_list = values.copy()
-        return sorted(set(new_list)) == values
-    
-
-    
-
-
-
-        
+        def valid_bst(root, left_bound, right_bound):
+            if (root is None):
+                return True
+            elif (root.val <= left_bound) or (root.val >= right_bound):
+                return False
+            return valid_bst(root.left, left_bound, root.val) and valid_bst(root.right, root.val,right_bound)
+        return valid_bst(root, float('-inf'), float('inf')) 
