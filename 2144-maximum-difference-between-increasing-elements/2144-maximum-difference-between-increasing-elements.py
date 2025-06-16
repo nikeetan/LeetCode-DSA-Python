6c3af9e4fb3bb07dc916d3c96fd2c7f745208fbe
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-15 21:04:07 -0500

    Time: 0 ms (100%), Space: 17.7 MB (98.88%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.py
@@ -2,14 +2,12 @@ class Solution:
     def maximumDifference(self, nums: List[int]) -> int:
         curr_indx = 1
         max_diff = float('-inf')
+        prev_min = nums[0]
         while curr_indx < len(nums):
             if nums[curr_indx] < nums[curr_indx - 1]:
-                curr_indx += 1
-                continue
-            else:
-                prev_indx = curr_indx - 1
-                while prev_indx >= 0 and nums[curr_indx] > nums[prev_indx]:
-                    max_diff = max(max_diff, nums[curr_indx] - nums[prev_indx])
-                    prev_indx -= 1
+                if nums[curr_indx] < prev_min:
+                    prev_min = nums[curr_indx]
+            elif nums[curr_indx] > nums[curr_indx - 1]:
+                max_diff = max(max_diff, nums[curr_indx] - nums[curr_indx - 1], nums[curr_indx] - prev_min)
             curr_indx += 1
         return max_diff if max_diff != float('-inf') else -1
