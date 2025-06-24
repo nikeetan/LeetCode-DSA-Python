97c310ec95e66d8e182a9408e96d7ba16d0fcdfc
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-24 08:20:39 -0500

    Time: 1869 ms (50.68%), Space: 18.5 MB (92.18%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -5,47 +5,52 @@ after that i will be adding one
 from collections import defaultdict
 class Solution:
     def countOfSubstrings(self, word: str, k: int) -> int:
-        vowel_set = {'a','e','i','o','u'}
-        vowel_counter = defaultdict(int)
-        consonants = 0
-        start, end = 0, 0
-        consonant_indices = [0] * len(word)
-        consonant_indx = len(word)
-        sub_strings = 0
+        #consonant_indices = [0] * len(word)
+        #consonant_indx = len(word)
+       
+        '''
         for i in range(len(word) - 1, -1, -1):
             consonant_indices[i] = consonant_indx
             if word[i] not in vowel_set:
                 consonant_indx = i
-        
-        while end < len(word):
-            if word[end] in vowel_set:
-                vowel_counter[word[end]] += 1 
-            else:
-                consonants += 1
-            
-            # consonants > k start shrinking
-            while consonants > k:
-                if word[start] in vowel_set:
-                    if vowel_counter[word[start]] - 1 == 0:
-                        del vowel_counter[word[start]]
-                    else:
-                        vowel_counter[word[start]] -= 1
+        '''
+        def helper(word, k):
+            vowel_set = {'a','e','i','o','u'}
+            vowel_counter = defaultdict(int)
+            consonants = 0
+            start, end = 0, 0
+            sub_strings = 0
+            while end < len(word):
+                if word[end] in vowel_set:
+                    vowel_counter[word[end]] += 1 
                 else:
-                    consonants -= 1
-                start += 1
-            
-            # when its a valid window that is consonants == k and vowels == 5
-            while consonants == k and len(vowel_counter) == 5 and start < len(word):
-                sub_strings += consonant_indices[end] - end
-                if word[start] in vowel_set:
-                    if vowel_counter[word[start]] - 1 == 0:
-                        del vowel_counter[word[start]]
+                    consonants += 1
+                '''
+                # consonants > k start shrinking
+                while consonants > k:
+                    if word[start] in vowel_set:
+                        if vowel_counter[word[start]] - 1 == 0:
+                            del vowel_counter[word[start]]
+                        else:
+                            vowel_counter[word[start]] -= 1
                     else:
-                        vowel_counter[word[start]] -= 1
-                else:
-                    consonants -= 1                    
-                start += 1
-            end += 1
-        return sub_strings
+                        consonants -= 1
+                    start += 1
+                '''
+                
+                # when its a valid window that is consonants == k and vowels == 5
+                while consonants >= k and len(vowel_counter) == 5 and start < len(word):
+                    sub_strings += len(word) - end
+                    if word[start] in vowel_set:
+                        if vowel_counter[word[start]] - 1 == 0:
+                            del vowel_counter[word[start]]
+                        else:
+                            vowel_counter[word[start]] -= 1
+                    else:
+                        consonants -= 1                    
+                    start += 1
+                end += 1
+            return sub_strings
+        return helper(word, k) - helper(word, k + 1)
 
 
