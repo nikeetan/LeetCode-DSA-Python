949804d34d4e45eece14e15ef4591e4837fb17be
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-06-30 09:48:36 -0500

    Time: 73 ms (10.7%), Space: 27.7 MB (6.36%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.py
@@ -5,18 +5,21 @@ i can think of this an n array tree
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
-        @lru_cache(None)
+        memo = {}
         def dfs(row, col):
+            if (row, col) in memo:
+                return memo[(row, col)]
             if col < 0 or col >= n:
                 return float('inf') 
             if row == n - 1:
                 return matrix[row][col]
-            
-            return matrix[row][col] + min(
+
+            memo[(row, col)] = matrix[row][col] + min(
                 dfs(row + 1, col),     
                 dfs(row + 1, col - 1), 
                 dfs(row + 1, col + 1)   
             )
+            return memo[(row, col)]
 
         return min(dfs(0, col) for col in range(n))
 
