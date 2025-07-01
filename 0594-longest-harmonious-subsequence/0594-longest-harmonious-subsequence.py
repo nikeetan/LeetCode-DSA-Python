50bbe3c39ddf5d7b03e70b2f8288bb50c7254fe0
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-07-01 12:07:45 -0500

    Time: 23 ms (77.01%), Space: 19 MB (72.13%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -10,24 +10,27 @@ class Solution:
                 numMap[i] = 1
             else:
                 numMap[i] += 1
-        nums.sort()
 
         # two pointers
-        firstPointer, secondPointer = 0, 1
-        subseqLen = 0
-        while secondPointer < len(nums):
-            if nums[secondPointer] - nums[firstPointer] == 1:
-                subseqLen = max(subseqLen, numMap[nums[firstPointer]] + numMap[nums[secondPointer]])
-            firstPointer += 1
-            secondPointer += 1
-        return subseqLen
+        subseq_count = 0
+        for key, val in numMap.items():
+            if key + 1 in numMap:
+                subseq_count = max(subseq_count, numMap[key] + numMap[key + 1])
+        return subseq_count
+        
+
 '''
 test case
 [1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,2,2,2,2]
-
 numMap = {1 : 1 , 2 : 3, 3 : 2, 5 : 1, 7 : 1}
 [1, 2,  2,  2,  3,  3,  5,  7]
                         fp  sp
 subseqLen = 5
+
+
+[-3,-1,-1,-1,-3,-2]
+
+[-3, -3, -2, -1, -1, -1]
+     fp.  sp
 
 '''
