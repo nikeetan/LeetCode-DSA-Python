b2f22209938483c3ec215099518e887cc23b6cf1
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-07-04 20:00:50 -0500

    Time: 29 ms (31.63%), Space: 47 MB (88.27%) - LeetHub

---
+++ 0697-degree-of-an-array/0697-degree-of-an-array.java
@@ -0,0 +1,41 @@
+/*
+Once i have a maximum occurance of different number than previous i can think of shrinking the window 
+
+*/
+import java.util.*;
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        HashMap<Integer, Integer> left = new HashMap<>();
+        HashMap<Integer, Integer> right = new HashMap<>();
+
+
+        for(int i = 0; i < nums.length; i ++)
+        {
+            if (!left.containsKey(nums[i])){
+                left.put(nums[i], i);
+            }
+            if (!right.containsKey(nums[i])){
+                right.put(nums[i], i);
+            }
+            else{
+                right.put(nums[i], i);
+            }
+            if (!count.containsKey(nums[i])){
+                count.put(nums[i], 1);
+            }
+            else{
+                count.put(nums[i], count.get(nums[i]) + 1);
+            }
+        }
+        int maxOccurance = Collections.max(count.values());
+        int length = Integer.MAX_VALUE;
+        for (int key : count.keySet())
+        {
+            if (count.get(key) == maxOccurance){
+                length = Math.min(length, (right.get(key) - left.get(key) + 1));
+            }
+        }
+    return length;
+    }
+}
