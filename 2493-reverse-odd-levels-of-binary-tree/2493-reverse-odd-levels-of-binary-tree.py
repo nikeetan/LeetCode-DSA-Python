e37d3a75fbf85fb0f6df6acdb760ba11e3ea7d01
Niketan Doddamani <61023399+nikeetan@users.noreply.github.com>
2025-08-11 18:54:15 -0500

    Time: 95 ms (9.89%), Space: 25.6 MB (5.42%) - LeetHub

---
+++ 2493-reverse-odd-levels-of-binary-tree/2493-reverse-odd-levels-of-binary-tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def reverseOddLevels(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        queue = deque()
+        queue.append(root)
+        odd_level = False
+        elements = []
+        while queue:
+            levelElements = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                levelElements.append(node.val)
+                if node.left and node.right:
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if odd_level:
+                levelElements = levelElements[::-1]
+                odd_level = False
+                elements.extend(levelElements)
+                continue
+            odd_level = True
+            elements.extend(levelElements)
+       
+        print(elements)
+       
+        #cnstruct binary tree is bascially dfs
+        newroot = TreeNode(0)
+        def constructBinary(indx : int):
+            if indx >= len(elements):
+                return None
+            newroot = TreeNode(elements[indx])
+            newroot.left = constructBinary(2 * indx + 1)
+            newroot.right = constructBinary(2 * indx + 2)
+            return newroot
+        return constructBinary(0)
+
